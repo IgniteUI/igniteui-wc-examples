ce4eeabe0b1326993f238a4e9c05f748582281d5
HUSSAR-mtrela <martin.trela@gmail.com>
2020-12-03 17:49:26 -0500

    ported MultiColumnComboBoxOverview sample

---
+++ samples/editors/multi-column-combobox/overview/src/MultiColumnComboBoxOverview.ts
@@ -0,0 +1,24 @@
+import { IgcMultiColumnComboBoxModule } from 'igniteui-webcomponents-grids';
+import { IgcMultiColumnComboBoxComponent, SortMode } from 'igniteui-webcomponents-grids';
+
+import { ModuleManager } from 'igniteui-webcomponents-core';
+import { SampleComboData } from './SampleComboData';
+
+ModuleManager.register(IgcMultiColumnComboBoxModule);
+
+export class MultiColumnComboBoxOverview {
+
+    private multiColumnComboBox: IgcMultiColumnComboBoxComponent;
+
+    constructor() {
+
+        this.multiColumnComboBox = document.getElementById("comboBox") as IgcMultiColumnComboBoxComponent;
+        this.multiColumnComboBox.dataSource = SampleComboData.getPopulation();
+        this.multiColumnComboBox.textField = "country";
+        this.multiColumnComboBox.sortMode = SortMode.SortByOneColumnOnly;
+        this.multiColumnComboBox.placeholder = "Choose a country";
+    }
+
+}
+
+let sample = new MultiColumnComboBoxOverview();
