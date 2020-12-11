129da763dbfa1f2198a665778feba47e0117f18b
HUSSAR-mtrela <martin.trela@gmail.com>
2020-12-11 14:56:22 -0500

    updates shape samples to use airplanes

--- samples/charts/data-chart/type-shape-polygon-series/src/DataChartTypeScatterPolygonSeries.ts
+++ samples/charts/data-chart/type-shape-polygon-series/src/DataChartTypeScatterPolygonSeries.ts
@@ -4,9 +4,12 @@ import { IgcDataChartShapeCoreModule } from 'igniteui-webcomponents-charts';
 import { IgcDataChartShapeModule } from 'igniteui-webcomponents-charts';
 import { IgcDataChartInteractivityModule } from 'igniteui-webcomponents-charts';
 import { IgcScatterPolygonSeriesModule } from 'igniteui-webcomponents-charts';
-import { IgcDataChartComponent } from 'igniteui-webcomponents-charts';
 import { IgcScatterPolygonSeriesComponent } from 'igniteui-webcomponents-charts';
-import { SampleShapeData } from './SampleShapeData';
+import { IgcStyleShapeEventArgs } from 'igniteui-webcomponents-charts';
+import { IgcShapeDataSource } from 'igniteui-webcomponents-core';
+import { IgcShapefileRecord } from 'igniteui-webcomponents-core';
+import { DataContext } from 'igniteui-webcomponents-core';
+import { html } from 'lit-html';
 
 ModuleManager.register(
     IgcDataChartCoreModule,
@@ -18,25 +21,100 @@ ModuleManager.register(
 
 export class DataChartTypeScatterPolygonSeries {
 
-    private chart: IgcDataChartComponent;
-
     constructor() {
 
-        this.chart = document.getElementById('chart') as IgcDataChartComponent;
-        this.chart.dataSource = SampleShapeData.create();
+        this.onAirplaneShapesLoaded = this.onAirplaneShapesLoaded.bind(this);
+        this.onAirplaneSeatsLoaded = this.onAirplaneSeatsLoaded.bind(this);
+        this.createTooltip = this.createTooltip.bind(this);
+
+        const url = 'https://static.infragistics.com/xplatform/shapes/airplanes/';
+
+        const airplaneShapeSource = new IgcShapeDataSource();
+        airplaneShapeSource.importCompleted = this.onAirplaneShapesLoaded;
+        airplaneShapeSource.shapefileSource = url + 'airplane-shape.shp';
+        airplaneShapeSource.databaseSource  = url + 'airplane-shape.dbf';
+        airplaneShapeSource.dataBind();
+
+        const airplaneSeatSource = new IgcShapeDataSource();
+        airplaneSeatSource.importCompleted = this.onAirplaneSeatsLoaded;
+        airplaneSeatSource.shapefileSource = url + 'airplane-seats.shp';
+        airplaneSeatSource.databaseSource  = url + 'airplane-seats.dbf';
+        airplaneSeatSource.dataBind();
     }
 
-    public setSeries() {
-        const series1 = new IgcScatterPolygonSeriesComponent();
-        series1.shapeMemberPath = 'Points';
-        series1.title = 'House Floor Plan';
-        series1.brush = 'Gray';
-        series1.outline = 'Black';
-        series1.xAxisName = 'xAxis';
-        series1.yAxisName = 'yAxis';
+    public onAirplaneShapesLoaded(sds: IgcShapeDataSource, e: any) {
+        console.log('onAirplaneShapesLoaded');
+        let airplaneShapes = sds.getPointData();
+        let airplaneShapeSeries = (document.getElementById('airplaneShapeSeries') as IgcScatterPolygonSeriesComponent);
+        airplaneShapeSeries.dataSource = airplaneShapes;
+        airplaneShapeSeries.renderSeries(false);
+    }
 
-        this.chart.series.clear();
-        this.chart.series.add(series1);
+    public onAirplaneSeatsLoaded(sds: IgcShapeDataSource, e: any) {
+        console.log('onAirplaneSeatsLoaded');
+        let airplaneShapes = sds.getPointData();
+        let airplaneSeatSeries = (document.getElementById('airplaneSeatSeries') as IgcScatterPolygonSeriesComponent);
+        airplaneSeatSeries.styleShape = this.onStylingShape;
+        airplaneSeatSeries.dataSource = airplaneShapes;
+        airplaneSeatSeries.tooltipTemplate = this.createTooltip;
+        airplaneSeatSeries.renderSeries(false);
+    }
+
+    public onStylingShape(sender: any, args: IgcStyleShapeEventArgs) {
+
+        args.shapeOpacity = 1.0;
+        args.shapeStrokeThickness = 0.5;
+        args.shapeStroke = "Black";
+
+        let itemRecord = args.item as IgcShapefileRecord;
+        if (itemRecord.fieldValues['Class'] === 'First Class') {
+            args.shapeFill = "DodgerBlue";
+        }
+        if (itemRecord.fieldValues['Class'] === 'Business Class') {
+            args.shapeFill = "LimeGreen";
+        }
+        if (itemRecord.fieldValues['Class'] === 'Premium Class') {
+            args.shapeFill = "Orange";
+        }
+        if (itemRecord.fieldValues['Class'] === 'Economy Class') {
+            args.shapeFill = "Red";
+        }
+
+        if (itemRecord.fieldValues['Status'] === 'Sold') {
+            args.shapeFill = 'Gray';
+        }
+    }
+
+    public createTooltip(context: any) {
+        const dataContext = context as DataContext;
+        if (!dataContext) return null;
+
+        const dataItem = dataContext.item as any;
+        if (!dataItem) return null;
+
+        let tooltip = html`<div>
+
+        <div style='display: 'inline-block', marginLeft: 5'>
+            <div class='tooltipBox'>
+                <div class='tooltipRow'>
+                    <div class='tooltipLbl'>Class</div>
+                    <div class='tooltipVal'>${dataItem.fieldValues['Class']}</div>
+                </div>
+                <div class='tooltipRow'>
+                    <div class='tooltipLbl'>Seat</div>
+                    <div class='tooltipVal'>${dataItem.fieldValues['Seat']} </div>
+                </div>
+                <div class='tooltipRow'>
+                    <div class='tooltipLbl'>Price</div>
+                    <div class='tooltipVal'>${dataItem.fieldValues['Price']}</div>
+                </div>
+                <div class='tooltipRow'>
+                    <div class='tooltipLbl'>Status</div>
+                    <div class='tooltipVal'>${dataItem.fieldValues['Status']}</div>
+                </div>
+            </div>
+        </div>`;
+        return tooltip;
     }
 }
 
