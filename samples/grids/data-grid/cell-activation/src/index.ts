06fa73b832f37d5dcc45fb83cbb9ab6ef4de9051
HUSSAR-mtrela <martin.trela@gmail.com>
2021-05-17 18:22:50 -0400

    redoing PR #51

--- samples/grids/data-grid/cell-activation/src/index.ts
+++ samples/grids/data-grid/cell-activation/src/index.ts
@@ -1,6 +1,8 @@
 import { IgcDataGridModule } from 'igniteui-webcomponents-grids';
 import { IgcGridColumnOptionsModule } from 'igniteui-webcomponents-grids';
 import { IgcDataGridComponent } from 'igniteui-webcomponents-grids';
+import { EnterKeyBehaviors } from 'igniteui-webcomponents-grids';
+import { EnterKeyBehaviorAfterEdit } from 'igniteui-webcomponents-grids';
 import { DataGridSharedData } from './DataGridSharedData';
 import { ModuleManager } from 'igniteui-webcomponents-core';
 
@@ -10,11 +12,40 @@ ModuleManager.register(IgcGridColumnOptionsModule);
 export class DataGridCellActivation {
 
     private grid: IgcDataGridComponent;
+    public enterBehaviorButton: any;
+    public enterKeyAfterEditButton: any;
 
     constructor() {
 
         this.grid = document.getElementById('grid') as IgcDataGridComponent;
         this.grid.dataSource = DataGridSharedData.getEmployees();
+        this.grid.enterBehaviorAfterEdit = EnterKeyBehaviorAfterEdit.MoveDown;
+
+        this.enterBehaviorButton = document.getElementById('enterModeDropBox');
+        if (this.enterBehaviorButton !== null){
+            this.enterBehaviorButton.onchange = this.enterModeChanged;
+        }
+
+        this.enterKeyAfterEditButton = document.getElementById('enterAfterEditMode');
+        if (this.enterKeyAfterEditButton !== null){
+            this.enterKeyAfterEditButton.onchange = this.enterAfterChanged;
+        }
+
+    }
+
+    public enterModeChanged = (event: any) => {
+
+        this.grid.enterBehavior = event.target.value;
+        if (this.grid.enterBehavior === EnterKeyBehaviors.Edit) {
+            this.enterKeyAfterEditButton.disabled = false;
+        }
+        else {
+            this.enterKeyAfterEditButton.disabled = true;
+        }
+    }
+
+    public enterAfterChanged = (event: any) => {
+        this.grid.enterBehaviorAfterEdit = event.target.value;
     }
 }
 
