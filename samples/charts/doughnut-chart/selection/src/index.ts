ae57281d1b76b19ef666223041ffbdfcc304c9f0
ig-robstoffers <rstoffers@infragistics.com>
2021-05-24 18:22:37 -0400

    fixing selection and explosion samples

--- samples/charts/doughnut-chart/selection/src/index.ts
+++ samples/charts/doughnut-chart/selection/src/index.ts
@@ -16,34 +16,41 @@ ModuleManager.register(
 export class DoughnutChartSelection {
 
     private chart: IgcDoughnutChartComponent;
+    private selectedSliceLabel: HTMLLabelElement;
+    private selectedSliceValue: HTMLLabelElement;
+    private data: any[];
 
     constructor() {
+        this.data = [
+            { MarketShare: 37, Category: "Cooling", Summary: "Cooling 37%", },
+            { MarketShare: 25, Category: "Residential", Summary: "Residential 25%",  },
+            { MarketShare: 12, Category: "Heating", Summary: "Heating 12%", },
+            { MarketShare: 8, Category: "Lighting", Summary: "Lighting 8%", },
+            { MarketShare: 18, Category: "Other", Summary: "Other 18%", }
+        ];
 
         let ringSeries = document.getElementById('ringSeries') as IgcRingSeriesComponent;
-
-        ringSeries.dataSource = this.getData();
+        ringSeries.dataSource = this.data;
         ringSeries.legend = document.getElementById('legend') as IgcItemLegendComponent
+        ringSeries.selectedSlices.add(0);
 
         this.chart = document.getElementById('chart') as IgcDoughnutChartComponent;
-        // this.chart.series.add(ringSeries);
         this.chart.sliceClick = this.onSliceClick;
-        this.chart.selectedSliceOpacity = 0.85;
-        this.chart.selectedSliceStrokeThickness = 4;
-        this.chart.selectedSliceStroke = 'rgba(0, 0, 0, 0.5)';
+
+        this.selectedSliceLabel = document.getElementById('selectedSliceLabel') as HTMLLabelElement;
+        this.selectedSliceLabel!.innerText = this.data[0].Category;
+        this.selectedSliceValue = document.getElementById('selectedSliceValue') as HTMLLabelElement;
+        this.selectedSliceValue!.innerText = this.data[0].MarketShare + "%";
     }
 
     public onSliceClick = (s: IgcDoughnutChartComponent, e: IgcSliceClickEventArgs) => {
-    }
-
-    public getData(): any[] {
-        return[
-            { MarketShare: 37, Company: "Space Cooling", Summary: "Space Cooling 37%" },
-            { MarketShare: 25, Company: "Residential Appliance", Summary: "Residential Appliance 25%"  },
-            { MarketShare: 12, Company: "Heating", Summary: "Heating 12%" },
-            { MarketShare: 8, Company: "Lighting", Summary: "Lighting 8%" },
-            { MarketShare: 18, Company: "Other Services", Summary: "Other Services 18%" }
-
-        ];
+        if (e.isSelected) {
+            this.selectedSliceLabel.innerText = this.data[e.index].Category;
+            this.selectedSliceValue.innerText = this.data[e.index].MarketShare + "%";
+        } else {
+            this.selectedSliceLabel.innerText = "No Selection";
+            this.selectedSliceValue.innerText = "0%";
+        }
     }
 }
 
