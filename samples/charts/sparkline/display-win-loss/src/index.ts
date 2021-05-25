87139e5c1a3331276f297013f2308f8bd708e8c9
ig-robstoffers <rstoffers@infragistics.com>
2021-05-25 18:17:12 -0400

    updating sparkline samples

--- samples/charts/sparkline/display-win-loss/src/index.ts
+++ samples/charts/sparkline/display-win-loss/src/index.ts
@@ -5,69 +5,47 @@ import { ModuleManager } from 'igniteui-webcomponents-core';
 ModuleManager.register(IgcSparklineModule);
 
 export class SparklineDisplayWinLoss {
-
     private chart1: IgcSparklineComponent;
     private chart2: IgcSparklineComponent;
     private chart3: IgcSparklineComponent;
-    public data1: any[] = [];
-    public data2: any[] = [];
+    public data: any[] = [];
 
     constructor() {
 
-        this.data1 = this.createData2(720 * 1);
-        this.data2 = this.createData2(720 * 2);
+        this.data = this.getData();
 
         this.chart1 = document.getElementById('chart1') as IgcSparklineComponent;
         this.chart2 = document.getElementById('chart2') as IgcSparklineComponent;
         this.chart3 = document.getElementById('chart3') as IgcSparklineComponent;
 
-        this.chart1.dataSource = this.data1;
-        this.chart2.dataSource = this.data1;
-        this.chart3.dataSource = this.data2;
+        this.chart1.dataSource = this.data;
+        this.chart2.dataSource = this.data;
+        this.chart3.dataSource = this.data;
     }
 
-    public createData1(itemsCount: number): any[] {
-        const data: any[] = [];
-        let v1 = 0;
-        let v2 = 5;
-        for (let i = 0; i <= itemsCount; i++) {
-            v1 += (Math.random() - 0.5) * 4;
-            v1 = this.clamp(v1, -10, 10);
-            v2 += (Math.random() - 0.5) * 4;
-            v2 = this.clamp(v2, -10, 10);
-            let l = i.toString();
-            data.push({ Label: l, Value1: v1, Value2: v2 });
-        }
-        return data;
-    }
-
-    public createData2(itemsCount: number): any[] {
+    public getData(): any[] {
         const data: any[] = [];
         let index = 0;
-        for (let angle = 0; angle <= itemsCount; angle += 10)
-        {
-            const r1 = Math.random() - 0.1;
-            const r2 = Math.random() - 0.2;
-            const v1 = Math.sin(angle * r1 * Math.PI / 180);
-            const v2 = Math.cos(angle * r2 * Math.PI / 180);
+        let min = 1000;
+        let max = -1000;
+
+        for (let angle = 0; angle <= 360 * 4; angle += 5) {
+            const v1 = Math.sin(angle * Math.PI / 180);
+            const v2 = Math.sin(3 * angle * Math.PI / 180) / 3;
+            let revenue = v1 + v2;
+            let expanse = revenue < 0 ? revenue : 0;
+            let income = revenue > 0 ? revenue : 0;
             data.push({
-                'Index': index++,
-                'Angle': angle,
-                'Value1': v1,
-                'Value2': v2,
+                Label: index++,
+                Angle: angle,
+                Revenue: revenue,
+                Expanse: expanse,
+                Income: income
             });
+            min = Math.min(min, revenue);
+            max = Math.max(max, revenue);
         }
         return data;
-    }
-
-    public clamp(v: number, min: number, max: number): number {
-        if (v > max) {
-            v = max;
-        }
-        else if (v < min) {
-            v = min;
-        }
-        return v;
     }
 }
 
