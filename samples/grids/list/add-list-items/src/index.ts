00f9e3ce4cc9cd71ab3ea7774bcb5a455744dae1
zhosti <IZhostov@infragistics.com>
2021-10-21 16:20:41 +0300

    chore(*): update list imports

--- samples/grids/list/add-list-items/src/index.ts
+++ samples/grids/list/add-list-items/src/index.ts
@@ -1,13 +1,7 @@
-import IgcListComponent from 'igniteui-webcomponents/src/components/list/list';
-import IgcListItemComponent from 'igniteui-webcomponents/src/components/list/list-item';
-import IgcListHeaderComponent from 'igniteui-webcomponents/src/components/list/list-header';
+import 'igniteui-webcomponents';
 
 export class AddListItems {
     constructor() {}
 }
-
-customElements.get('igc-list') || customElements.define('igc-list', IgcListComponent);
-customElements.get('igc-list-item') || customElements.define('igc-list-item', IgcListItemComponent);
-customElements.get('igc-list-header') || customElements.define('igc-list-header', IgcListHeaderComponent);
 
 new AddListItems();
--- samples/grids/list/styling/src/index.ts
+++ samples/grids/list/styling/src/index.ts
@@ -1,18 +1,8 @@
-import IgcListComponent from 'igniteui-webcomponents/src/components/list/list';
-import IgcListItemComponent from 'igniteui-webcomponents/src/components/list/list-item';
-import IgcListHeaderComponent from 'igniteui-webcomponents/src/components/list/list-header';
-import IgcAvatarComponent from 'igniteui-webcomponents/src/components/avatar/avatar';
-import IgcButtonComponent from 'igniteui-webcomponents/src/components/button/button';
+import 'igniteui-webcomponents';
 import './ListStyling.css';
 
 export class ListStyling{
     constructor() {}
 }
-
-customElements.get('igc-list') || customElements.define('igc-list', IgcListComponent);
-customElements.get('igc-list-item') || customElements.define('igc-list-item', IgcListItemComponent);
-customElements.get('igc-list-header') || customElements.define('igc-list-header', IgcListHeaderComponent);
-customElements.get('igc-avatar') ||customElements.define('igc-avatar', IgcAvatarComponent);
-customElements.get('igc-button') || customElements.define('igc-button', IgcButtonComponent);
 
 new ListStyling();
