786c410963a03db9e4952b90e7d941cb664e19cf
igdmdimitrov <dmdimitrov@infragistics.com>
2021-10-28 13:07:58 +0300

    feat(navbar): add samples for navbar - vNext

---
+++ samples/menus/navbar/styling/src/index.ts
@@ -0,0 +1,9 @@
+import 'igniteui-webcomponents';
+import 'igniteui-webcomponents/src/styles/themes/material.css';
+import './NavbarStyling.css';
+
+export class NavbarStyling {
+    constructor() { }
+}
+
+new NavbarStyling();
