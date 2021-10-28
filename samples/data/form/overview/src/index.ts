fc76971464f7a434b8f04a01c1ff82e01494d6c1
igdmdimitrov <dmdimitrov@infragistics.com>
2021-10-28 13:42:05 +0300

    sample(form): updated packages and sample

--- samples/data/form/overview/src/index.ts
+++ samples/data/form/overview/src/index.ts
@@ -1,15 +1,8 @@
-
-import IgcButtonComponent from 'igniteui-webcomponents/src/components/button/button';
-import IgcCheckboxComponent from 'igniteui-webcomponents/src/components/checkbox/checkbox';
-import IgcFormComponent from 'igniteui-webcomponents/src/components/form/form';
-import IgcInputComponent from 'igniteui-webcomponents/src/components/input/input';
+import 'igniteui-webcomponents';
 
 export class FormOverview {
-    private form: IgcFormComponent;
-
     constructor() {
-        this.form = document.getElementById("form") as unknown as IgcFormComponent;
-        this.form.addEventListener('igcSubmit', function (event) {
+        document.addEventListener('igcSubmit', function (event) {
             const customEvent = event as CustomEvent<FormData>;
             const formData = customEvent.detail;
             let result = '';
@@ -22,9 +15,5 @@ export class FormOverview {
     }
 }
 
-customElements.get('igc-button') || customElements.define('igc-button', IgcButtonComponent);
-customElements.get('igc-checkbox') || customElements.define('igc-checkbox', IgcCheckboxComponent);
-customElements.get('igc-form') || customElements.define('igc-form', IgcFormComponent);
-customElements.get('igc-input') || customElements.define('igc-input', IgcInputComponent);
 
 new FormOverview();
