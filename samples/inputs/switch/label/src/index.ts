f5ec648bdafc5ececaac1361d1e3ef8f0dd0f0ea
HUSSAR-mtrela <martin.trela@gmail.com>
2021-11-02 17:09:35 -0400

    removed import of empty index.css

--- samples/inputs/switch/label/src/index.ts
+++ samples/inputs/switch/label/src/index.ts
@@ -1,6 +1,5 @@
 import 'igniteui-webcomponents/src/components/checkbox/switch';
 import 'igniteui-webcomponents/src/styles/themes/material.css';
-import './index.css'
 
 export class SwitchLabel {}
 
