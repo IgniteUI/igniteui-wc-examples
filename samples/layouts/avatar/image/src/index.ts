62a45be8a18e38e29ddd0b90bf2c3e609d9a1d11
Silvia Ivanova <SIvanova@infragistics.com>
2021-11-04 16:57:33 +0200

    refactor(samples): update package and imports

--- samples/layouts/avatar/image/src/index.ts
+++ samples/layouts/avatar/image/src/index.ts
@@ -1,7 +1,6 @@
-import 'igniteui-webcomponents/src/components/avatar/avatar';
-import 'igniteui-webcomponents/src/components/icon/icon';
-import { registerIcon } from 'igniteui-webcomponents/src/components/icon/icon.registry';
-import 'igniteui-webcomponents/src/styles/themes/material.css';
+import 'igniteui-webcomponents';
+import { registerIcon } from 'igniteui-webcomponents';
+import 'igniteui-webcomponents/themes/material.css';
 
 export class ImageAvatar {
     constructor() {
