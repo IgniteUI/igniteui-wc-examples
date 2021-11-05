aa7b344b108d0488c1f2bef1f566c8f667e7f595
Simeon Simeonoff <sim.simeonoff@gmail.com>
2021-11-05 10:17:21 +0200

    refactor(*): update igniteui-webcomponents package

--- samples/inputs/radio/disabled/src/index.ts
+++ samples/inputs/radio/disabled/src/index.ts
@@ -1,6 +1,5 @@
-import "igniteui-webcomponents/src/components/radio/radio";
-import "igniteui-webcomponents/src/components/radio-group/radio-group";
-import "igniteui-webcomponents/src/styles/themes/material.css";
+import "igniteui-webcomponents";
+import "igniteui-webcomponents/themes/material.css";
 
 export class RadioDisabled {
   constructor() {}
