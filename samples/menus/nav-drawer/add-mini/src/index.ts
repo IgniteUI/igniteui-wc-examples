135f5fe38fce02ece4c8402fde540cb8373eeb1c
IBarakov <ibarakov@infragistics.com>
2021-11-15 02:43:10 +0200

    sync items mini and nav drawer items

--- samples/menus/nav-drawer/add-mini/src/index.ts
+++ samples/menus/nav-drawer/add-mini/src/index.ts
@@ -1,7 +1,6 @@
 import { defineComponents, IgcNavDrawerComponent, IgcNavDrawerHeaderItemComponent, IgcNavDrawerItemComponent,
   IgcButtonComponent, IgcIconComponent, registerIcon } from 'igniteui-webcomponents';
 import 'igniteui-webcomponents/themes/bootstrap.css';
-
 defineComponents(IgcNavDrawerComponent, IgcNavDrawerHeaderItemComponent, IgcNavDrawerItemComponent,
   IgcButtonComponent, IgcIconComponent);
 export class NavDrawerAddMini {
@@ -16,6 +15,32 @@ export class NavDrawerAddMini {
       document.getElementById('root')!.onclick = (e) => {
         if(e.target === document.getElementById('content')) {
           navDrawer.hide();
+        }
+      }
+
+      navDrawer.onclick = (e) => {
+        const el = e.target as HTMLElement;     
+        const targetElTagName = el.tagName.toLowerCase();
+        const targetElParentTagName = el.parentElement?.tagName.toLowerCase();
+        const mini = document.querySelector('div[slot="mini"]');
+
+        const drawerItem = targetElTagName === "igc-nav-drawer-item" ?
+          el :
+          targetElParentTagName === "igc-nav-drawer-item" ? el.parentElement : null;
+
+        if (mini !== null && mini.children.length > 0 && drawerItem) {
+          const iconName = drawerItem.querySelector('igc-icon')?.getAttribute('name');
+          const iconButtons = document.querySelectorAll(`igc-icon`);
+          
+          iconButtons.forEach(b => {
+            const parentEl = b.parentElement! as IgcNavDrawerItemComponent;
+
+            if(b.getAttribute('name') === iconName) {
+              parentEl.active = true;
+            } else {
+              parentEl.active = false;
+            }
+          })
         }
       }
 
