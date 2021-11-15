db506744cb20507571a7a0fb3069c5bff6b918ff
Diyan Dimitrov <ddimitrov@infragistics.com>
2021-11-15 13:31:38 +0200

    optimize mini nav drawer sample

--- samples/menus/nav-drawer/add-mini/src/index.ts
+++ samples/menus/nav-drawer/add-mini/src/index.ts
@@ -1,6 +1,7 @@
 import { defineComponents, IgcNavDrawerComponent, IgcNavDrawerHeaderItemComponent, IgcNavDrawerItemComponent,
   IgcButtonComponent, IgcIconComponent, registerIcon } from 'igniteui-webcomponents';
 import 'igniteui-webcomponents/themes/bootstrap.css';
+
 defineComponents(IgcNavDrawerComponent, IgcNavDrawerHeaderItemComponent, IgcNavDrawerItemComponent,
   IgcButtonComponent, IgcIconComponent);
 export class NavDrawerAddMini {
@@ -19,26 +20,20 @@ export class NavDrawerAddMini {
       }
 
       navDrawer.onclick = (e) => {
-        const el = e.target as HTMLElement;     
-        const targetElTagName = el.tagName.toLowerCase();
-        const targetElParentTagName = el.parentElement?.tagName.toLowerCase();
-        const mini = document.querySelector('div[slot="mini"]');
-
-        const drawerItem = targetElTagName === "igc-nav-drawer-item" ?
-          el :
-          targetElParentTagName === "igc-nav-drawer-item" ? el.parentElement : null;
-
-        if (mini !== null && mini.children.length > 0 && drawerItem) {
-          const iconName = drawerItem.querySelector('igc-icon')?.getAttribute('name');
-          const iconButtons = document.querySelectorAll(`igc-icon`);
+        const target = e.target as HTMLElement;
+        const drawerItem = target.closest('igc-nav-drawer-item');
+     
+        if (drawerItem) {
+          const iconName = drawerItem.querySelector('igc-icon')?.name;
+          const icons = document.querySelectorAll(`igc-icon`);
           
-          iconButtons.forEach(b => {
-            const parentEl = b.parentElement! as IgcNavDrawerItemComponent;
+          icons.forEach(icon => {
+            const parentItem = icon.parentElement! as IgcNavDrawerItemComponent;
 
-            if(b.getAttribute('name') === iconName) {
-              parentEl.active = true;
+            if(icon.name === iconName) {
+              parentItem.active = true;
             } else {
-              parentEl.active = false;
+              parentItem.active = false;
             }
           })
         }
