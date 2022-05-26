5f81175a328064fc90aa76a55b903d5a5e51ba66
ddaribo <bozhidara.pp@gmail.com>
2022-05-26 14:18:26 +0300

    fix(tree-sample): remove unused import

--- samples/grids/tree/basic-example/src/index.ts
+++ samples/grids/tree/basic-example/src/index.ts
@@ -1,6 +1,5 @@
 import { defineComponents, IgcTreeComponent, IgcTreeItemComponent } from "igniteui-webcomponents";
 import "igniteui-webcomponents/themes/light/bootstrap.css";
-import { DATA } from "./AnimationsData";
 import "./TreeBasic.css";
 
 defineComponents(IgcTreeComponent, IgcTreeItemComponent);
