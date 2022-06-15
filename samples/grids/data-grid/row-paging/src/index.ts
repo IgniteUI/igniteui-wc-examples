a9bd9026f3a243678140fde7fba1ef153b634b3c
mddifilippo89 <mdifilippo@infragistics.com>
2022-06-15 15:53:16 -0400

    row paging (#272)
    add row paging

--- samples/grids/data-grid/row-paging/src/index.ts
+++ samples/grids/data-grid/row-paging/src/index.ts
@@ -1,20 +1,20 @@
+import { DataGridSharedData } from './DataGridSharedData';
+import { ModuleManager } from 'igniteui-webcomponents-core';
 import { IgcDataGridModule } from 'igniteui-webcomponents-grids';
 import { IgcDataGridComponent } from 'igniteui-webcomponents-grids';
-import { ModuleManager } from 'igniteui-webcomponents-core';
 
-ModuleManager.register(IgcDataGridModule);
+ModuleManager.register(
+    IgcDataGridModule,
+);
 
 export class DataGridRowPaging {
 
     private grid: IgcDataGridComponent;
-
     constructor() {
 
         this.grid = document.getElementById('grid') as IgcDataGridComponent;
-
-        // TODO set property settings (if any) in code-behind:
+        this.grid.dataSource = DataGridSharedData.getEmployees(100);
     }
-
 }
 
 new DataGridRowPaging();
