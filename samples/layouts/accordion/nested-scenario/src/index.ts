415ca8905f1675e319bb7bab33f6b6e299665e1c
ddaribo <bozhidara.pp@gmail.com>
2022-07-19 10:32:13 +0300

    refactor(accordion): address comments

--- samples/layouts/accordion/nested-scenario/src/index.ts
+++ samples/layouts/accordion/nested-scenario/src/index.ts
@@ -1,20 +1,20 @@
-import { defineComponents, IgcAccordionComponent, IgcExpansionPanelComponent, IgcSwitchComponent } from 'igniteui-webcomponents';
-import 'igniteui-webcomponents/themes/light/bootstrap.css';
-import './AccordionNestedScenario.css';
+import { defineComponents, IgcAccordionComponent, IgcExpansionPanelComponent, IgcSwitchComponent } from "igniteui-webcomponents";
+import "igniteui-webcomponents/themes/light/bootstrap.css";
+import "./AccordionNestedScenario.css";
 
-defineComponents(IgcAccordionComponent, IgcExpansionPanelComponent, IgcSwitchComponent );
+defineComponents(IgcAccordionComponent, IgcExpansionPanelComponent, IgcSwitchComponent);
 export class AccordionNestedScenario {
-	private accordion: IgcAccordionComponent;
-	private switch: IgcSwitchComponent;
+    private accordion: IgcAccordionComponent;
+    private switch: IgcSwitchComponent;
 
-	constructor() {
-		this.accordion = document.getElementById("accordion") as IgcAccordionComponent;
-		this.switch = document.getElementById("switch") as IgcSwitchComponent;
+    constructor() {
+        this.accordion = document.getElementById("accordion") as IgcAccordionComponent;
+        this.switch = document.getElementById("switch") as IgcSwitchComponent;
 
-		this.switch.addEventListener("igcChange", (ev: CustomEvent) => {
-			this.accordion.singleExpand = ev.detail;
-		});
-	}
+        this.switch.addEventListener("igcChange", (ev: CustomEvent) => {
+            this.accordion.singleExpand = ev.detail;
+        });
+    }
 }
 
 new AccordionNestedScenario();
--- samples/layouts/accordion/overview/src/index.ts
+++ samples/layouts/accordion/overview/src/index.ts
@@ -1,20 +1,20 @@
-import { defineComponents, IgcAccordionComponent, IgcSwitchComponent } from 'igniteui-webcomponents';
-import 'igniteui-webcomponents/themes/light/bootstrap.css';
-import './AccordionOverview.css';
+import { defineComponents, IgcAccordionComponent, IgcExpansionPanelComponent, IgcSwitchComponent } from "igniteui-webcomponents";
+import "igniteui-webcomponents/themes/light/bootstrap.css";
+import "./AccordionOverview.css";
 
-defineComponents(IgcAccordionComponent, IgcSwitchComponent );
+defineComponents(IgcAccordionComponent, IgcExpansionPanelComponent, IgcSwitchComponent);
 export class AccordionOverview {
-	private accordion: IgcAccordionComponent;
-	private switch: IgcSwitchComponent;
+    private accordion: IgcAccordionComponent;
+    private switch: IgcSwitchComponent;
 
-	constructor() {
-		this.accordion = document.getElementById("accordion") as IgcAccordionComponent;
-		this.switch = document.getElementById("switch") as IgcSwitchComponent;
+    constructor() {
+        this.accordion = document.getElementById("accordion") as IgcAccordionComponent;
+        this.switch = document.getElementById("switch") as IgcSwitchComponent;
 
-		this.switch.addEventListener("igcChange", (ev: CustomEvent) => {
-			this.accordion.singleExpand = ev.detail;
-		});
-	}
+        this.switch.addEventListener("igcChange", (ev: CustomEvent) => {
+            this.accordion.singleExpand = ev.detail;
+        });
+    }
 }
 
 new AccordionOverview();
