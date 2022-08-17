1112de4e419ac1352861348505db44eadefb2eb2
Stamen Stoychev <sstoychev@infragistics.com>
2022-08-17 23:55:44 +0300

    resolving issues with rating samples (#303)

--- samples/inputs/rating/single-selection/src/index.ts
+++ samples/inputs/rating/single-selection/src/index.ts
@@ -1,5 +1,6 @@
 import { defineComponents, IgcRatingComponent } from 'igniteui-webcomponents';
 import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import './index.css';
 
 defineComponents(IgcRatingComponent);
 export class SingleSelectionRating {
