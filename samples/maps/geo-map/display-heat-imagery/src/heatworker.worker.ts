cc150038a3d19cd91dbd28a50c78b80aab01df2e
Michael DiFilippo <mdifilippo@infragistics.com>
2022-11-02 09:31:36 -0400

    Update heatworker.worker.ts

--- samples/maps/geo-map/display-heat-imagery/src/heatworker.worker.ts
+++ samples/maps/geo-map/display-heat-imagery/src/heatworker.worker.ts
@@ -1,13 +1,16 @@
 import { HeatTileGeneratorWebWorker } from 'igniteui-webcomponents-core';
 
 let worker: Worker = self as any;
-worker.onmessage = HeatTileGeneratorWebWorker.onmessage;
+if (typeof worker !== "undefined") {
 
-HeatTileGeneratorWebWorker.postmessage = postMessageFunction;
-HeatTileGeneratorWebWorker.start();
+    worker.onmessage = HeatTileGeneratorWebWorker.onmessage;
 
-function postMessageFunction() {
-    self.postMessage.apply(self, Array.prototype.slice.call(arguments));
+    HeatTileGeneratorWebWorker.postmessage = postMessageFunction;
+    HeatTileGeneratorWebWorker.start();
+
+    function postMessageFunction() {
+        self.postMessage.apply(self, Array.prototype.slice.call(arguments) as any);
+    }
 }
 
 export default {} as typeof Worker & (new () => Worker);
