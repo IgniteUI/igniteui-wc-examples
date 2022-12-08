2e728cc93411cfae07507ad1f23af960db4133bd
HUSSAR-mtrela <martin.trela@gmail.com>
2022-12-08 15:34:07 -0500

    export grid samples

---
+++ samples/grids/grid/external-advanced-filtering/src/index.ts
@@ -0,0 +1,62 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import 'igniteui-webcomponents-grids/grids/combined';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcGridComponent, IgcColumnComponent } from 'igniteui-webcomponents-grids/grids';
+import { NwindDataItem, NwindDataItem_LocationsItem, NwindData } from './NwindData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import { ModuleManager } from 'igniteui-webcomponents-core';
+
+ModuleManager.register(
+    IgcPropertyEditorPanelModule
+);
+
+export class Sample {
+
+    private grid: IgcGridComponent
+    private productName: IgcColumnComponent
+    private quantityPerUnit: IgcColumnComponent
+    private unitPrice: IgcColumnComponent
+    private orderDate: IgcColumnComponent
+    private discontinued: IgcColumnComponent
+    private _bind: () => void;
+
+    constructor() {
+        var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
+        var productName = this.productName = document.getElementById('ProductName') as IgcColumnComponent;
+        var quantityPerUnit = this.quantityPerUnit = document.getElementById('QuantityPerUnit') as IgcColumnComponent;
+        var unitPrice = this.unitPrice = document.getElementById('UnitPrice') as IgcColumnComponent;
+        var orderDate = this.orderDate = document.getElementById('OrderDate') as IgcColumnComponent;
+        var discontinued = this.discontinued = document.getElementById('Discontinued') as IgcColumnComponent;
+
+        this._bind = () => {
+            grid.data = this.nwindData
+        }
+        this._bind();
+
+    }
+
+    private _nwindData: NwindData = null;
+    public get nwindData(): NwindData {
+        if (this._nwindData == null)
+        {
+            this._nwindData = new NwindData();
+        }
+        return this._nwindData;
+    }
+    
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            WebGridDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
+new Sample();
---
+++ samples/grids/grid/layout-display-density/src/index.ts
@@ -0,0 +1,62 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import 'igniteui-webcomponents-grids/grids/combined';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcPropertyEditorPanelComponent, IgcPropertyEditorPropertyDescriptionComponent } from 'igniteui-webcomponents-layouts';
+import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { InvoicesDataItem, InvoicesData } from './InvoicesData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import { defineAllComponents } from 'igniteui-webcomponents';
+import { ModuleManager } from 'igniteui-webcomponents-core';
+defineAllComponents();
+
+ModuleManager.register(
+    IgcPropertyEditorPanelModule
+);
+
+export class Sample {
+
+    private propertyEditor: IgcPropertyEditorPanelComponent
+    private displayDensityEditor: IgcPropertyEditorPropertyDescriptionComponent
+    private grid: IgcGridComponent
+    private _bind: () => void;
+
+    constructor() {
+        var propertyEditor = this.propertyEditor = document.getElementById('PropertyEditor') as IgcPropertyEditorPanelComponent;
+        var displayDensityEditor = this.displayDensityEditor = document.getElementById('DisplayDensityEditor') as IgcPropertyEditorPropertyDescriptionComponent;
+        var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
+
+        this._bind = () => {
+            propertyEditor.componentRenderer = this.renderer
+            propertyEditor.target = this.grid
+            grid.data = this.invoicesData
+        }
+        this._bind();
+
+    }
+
+    private _invoicesData: InvoicesData = null;
+    public get invoicesData(): InvoicesData {
+        if (this._invoicesData == null)
+        {
+            this._invoicesData = new InvoicesData();
+        }
+        return this._invoicesData;
+    }
+    
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            WebGridDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
+new Sample();
