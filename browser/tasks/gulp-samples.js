d02086e443916d1dc262397f8241c7b97fb4a812
HUSSAR-mtrela <martin.trela@gmail.com>
2022-12-14 11:12:16 -0500

    fixed script

--- browser/tasks/gulp-samples.js
+++ browser/tasks/gulp-samples.js
@@ -402,7 +402,7 @@ function copySamples(cb) {
 function updateSampleReadme(cb) {
 
     // log('updating readme files... ');
-    var template = fs.readFileSync("./templates/sample/ReadMe.md", "utf8");
+    var template = fs.readFileSync("./browser/templates/sample/ReadMe.md", "utf8");
     for (const sample of samples) {
 
         // let outputPath = sampleOutputFolder + '/' + sample.SampleFolderPath;
