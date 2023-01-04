4409511ebbcc5219f36ac7d99b039071506e9236
Martin Dragnev <37667452+mddragnev@users.noreply.github.com>
2023-01-05 01:10:16 +0200

    Add Pivot Remote sample (#343)
    * Add initial grid
    
    * Add working noop pivot grid
    
    * Finalize sample
    
    * Update web components version and correct pivot property
    
    * update imports
    
    Co-authored-by: HUSSAR-mtrela <HUSSAR-mtrela@users.noreply.github.com>

---
+++ samples/grids/pivot-grid/remote/src/index.ts
@@ -0,0 +1,74 @@
+import '@infragistics/igniteui-webcomponents-grids/grids/combined';
+import { IgcPivotGridComponent, NoopPivotDimensionsStrategy } from '@infragistics/igniteui-webcomponents-grids/grids';
+import "@infragistics/igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import { PivotNoopData } from './PivotNoopData';
+
+export class Sample {
+    private _pivotConfiguration: any | null = null;
+    public get pivotConfiguration(): any {
+        if (this._pivotConfiguration == null) {
+            const pivotConfiguration: any = {
+                columnStrategy: NoopPivotDimensionsStrategy.instance(),
+                rowStrategy: NoopPivotDimensionsStrategy.instance(),
+                columns: [
+                    {
+                        memberName: 'Country',
+                        enabled: true
+                    }
+                ],
+                rows: [
+                    {
+                        memberFunction: () => 'All',
+                        memberName: 'AllProducts',
+                        enabled: true,
+                        childLevel: {
+                            memberFunction: (data: any) => data.ProductCategory,
+                            memberName: 'ProductCategory',
+                            enabled: true
+                        }
+                    },
+                    {
+                        memberName: 'AllSeller',
+                        memberFunction: () => 'All Sellers',
+                        enabled: true,
+                        childLevel: {
+                            enabled: true,
+                            memberName: 'SellerName'
+                        }
+                    }
+                ],
+                values: [
+                    {
+                        member: 'UnitsSold',
+                        aggregate: {
+                            aggregatorName: 'SUM',
+                            key: 'sum',
+                            label: 'Sum'
+                        },
+                        enabled: true,
+                        formatter: (value: any) => value ? value : 0
+                    }
+                ],
+                filters: null
+            };
+
+            this._pivotConfiguration = pivotConfiguration;
+        }
+        return this._pivotConfiguration;
+    }
+    private _bind: () => void;
+
+    constructor() {
+        var grid = document.getElementById('grid') as IgcPivotGridComponent;
+
+        this._bind = () => {
+            grid.pivotConfiguration = this.pivotConfiguration;
+            PivotNoopData.getData().then(value => {
+                grid.data = value;
+            });
+        }
+        this._bind();
+    }
+}
+
+new Sample();
