6066bee1904cd0df98ffca94b69af5139e23bc6f
HUSSAR-mtrela <martin.trela@gmail.com>
2023-03-29 13:04:28 -0400

    export fix for tree grid

--- samples/grids/tree-grid/editing-events/src/index.ts
+++ samples/grids/tree-grid/editing-events/src/index.ts
@@ -42,7 +42,7 @@ export class Sample {
         return this._componentRenderer;
     }
 
-    public webGridEditingEventsCellEdit(args: any): void {
+    public WebTreeGridEditingEventsCellEdit(args: any): void {
         var d = args.detail;
 
         if (d.column != null && d.column.field == "Name") {
