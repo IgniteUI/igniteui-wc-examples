22debe29e548795609ebed9be17b679ed810d633
Maya Kirova <MKirova@infragistics.com>
2023-04-21 17:25:20 +0300

    Add cell selection styling sample.

---
+++ samples/grids/grid/cell-selection-style/src/index.ts
@@ -0,0 +1,38 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { NwindDataItem, NwindDataItem_LocationsItem, NwindData } from './NwindData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import { defineAllComponents } from 'igniteui-webcomponents';
+
+import "./index.css";
+
+defineAllComponents();
+
+export class Sample {
+    private _bind: () => void;
+
+    constructor() {
+        var grid = document.getElementById('grid') as IgcGridComponent;
+
+        this._bind = () => {
+            grid.data = this.nwindData;
+        }
+        this._bind();
+
+    }
+
+    private _nwindData: NwindData = null;
+    public get nwindData(): NwindData {
+        if (this._nwindData == null)
+        {
+            this._nwindData = new NwindData();
+        }
+        return this._nwindData;
+    }
+
+}
+
+new Sample();
