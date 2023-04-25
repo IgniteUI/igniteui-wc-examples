a1a22bcbbaec958d764a1e377fa9593ab303b605
Martin Dragnev <mddragnev@gmail.com>
2023-04-25 12:03:28 +0300

    Add advanced filtering styling sample

---
+++ samples/grids/grid/advanced-filtering-style/src/index.ts
@@ -0,0 +1,57 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { ComponentRenderer, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcGridComponent, IgcColumnComponent } from 'igniteui-webcomponents-grids/grids';
+import { NwindDataItem, NwindDataItem_LocationsItem, NwindData } from './NwindData';
+import { IgcCellTemplateContext } from 'igniteui-webcomponents-grids/grids';
+import { html, nothing } from 'lit-html';
+import "./index.css";
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+export class Sample {
+
+    private grid: IgcGridComponent
+    private column1: IgcColumnComponent
+    private _bind: () => void;
+
+    constructor() {
+        var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
+        var column1 = this.column1 = document.getElementById('column1') as IgcColumnComponent;
+
+        this._bind = () => {
+            grid.data = this.nwindData;
+            column1.bodyTemplate = this.webGridDiscontinuedCellTemplate;
+        }
+        this._bind();
+
+    }
+
+    private _nwindData: NwindData = null;
+    public get nwindData(): NwindData {
+        if (this._nwindData == null)
+        {
+            this._nwindData = new NwindData();
+        }
+        return this._nwindData;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            WebGridDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+    public webGridDiscontinuedCellTemplate = (ctx: IgcCellTemplateContext) => {
+        if (ctx.cell.value) {
+            return html`<img src="https://www.infragistics.com/angular-demos-lob/assets/images/grid/active.png" title="Continued" alt="Continued" />`
+        } else {
+            return html`<img src="https://www.infragistics.com/angular-demos-lob/assets/images/grid/expired.png" title="Discontinued" alt="Discontinued" />`;
+        }
+    };
+
+}
+
+new Sample();
