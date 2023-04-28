2e83435b4c09bc7104fd86d5f2a0e6d66c9f8596
Maya Kirova <MKirova@infragistics.com>
2023-04-28 16:49:32 +0300

    Add custom filtering sample for wc.

---
+++ samples/grids/grid/custom-filtering/src/index.ts
@@ -0,0 +1,102 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcGridComponent, IgcColumnComponent, IgcStringFilteringOperand, IgcFilteringOperand, IgcFilteringOperation, IgcBooleanFilteringOperand } from 'igniteui-webcomponents-grids/grids';
+import { NwindDataItem, NwindDataItem_LocationsItem, NwindData } from './NwindData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+
+
+export class Sample {
+
+    private grid: IgcGridComponent
+    private productName: IgcColumnComponent
+    private quantityPerUnit: IgcColumnComponent
+    private unitPrice: IgcColumnComponent
+    private orderDate: IgcColumnComponent
+    private discontinued: IgcColumnComponent
+    private _bind: () => void;
+    public caseSensitiveFilteringOperand = CaseSensitiveFilteringOperand.instance();
+    public booleanFilteringOperand = BooleanFilteringOperand.instance();
+
+    constructor() {
+        var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
+        var productName = this.productName = document.getElementById('ProductName') as IgcColumnComponent;
+        var quantityPerUnit = this.quantityPerUnit = document.getElementById('QuantityPerUnit') as IgcColumnComponent;
+        var unitPrice = this.unitPrice = document.getElementById('UnitPrice') as IgcColumnComponent;
+        var orderDate = this.orderDate = document.getElementById('OrderDate') as IgcColumnComponent;
+        var discontinued = this.discontinued = document.getElementById('Discontinued') as IgcColumnComponent;
+
+        this._bind = () => {
+            grid.data = this.nwindData;
+            productName.filters = this.caseSensitiveFilteringOperand;
+            discontinued.filters = this.booleanFilteringOperand;
+        }
+        this._bind();
+
+    }
+
+    private _nwindData: NwindData = null;
+    public get nwindData(): NwindData {
+        if (this._nwindData == null)
+        {
+            this._nwindData = new NwindData();
+        }
+        return this._nwindData;
+    }
+}
+
+export class CaseSensitiveFilteringOperand extends IgcStringFilteringOperand {
+    private constructor() {
+        super();
+        const customOperations = [
+            {
+                iconName: 'contains',
+                isUnary: false,
+                logic: (target: string, searchVal: string, ignoreCase?: boolean) => {
+                    ignoreCase = false;
+                    const search = IgcStringFilteringOperand.applyIgnoreCase(searchVal, ignoreCase);
+                    target = IgcStringFilteringOperand.applyIgnoreCase(target, ignoreCase);
+                    return target.indexOf(search) !== -1;
+                },
+                name: 'Contains (case sensitive)'
+            },
+            {
+                iconName: 'does_not_contain',
+                isUnary: false,
+                logic: (target: string, searchVal: string, ignoreCase?: boolean) => {
+                    ignoreCase = false;
+                    const search = IgcStringFilteringOperand.applyIgnoreCase(searchVal, ignoreCase);
+                    target = IgcStringFilteringOperand.applyIgnoreCase(target, ignoreCase);
+                    return target.indexOf(search) === -1;
+                },
+                name: 'Does Not Contain (case sensitive)'
+            }
+        ];
+
+        const emptyOperators = [
+            // 'Empty'
+            this.operations[6],
+            // 'Not Empty'
+            this.operations[7]
+        ];
+
+        this.operations = customOperations.concat(emptyOperators);
+    }
+}
+
+export class BooleanFilteringOperand extends IgcBooleanFilteringOperand {
+    private constructor() {
+        super();
+        this.operations = [
+            // 'All'
+            this.operations[0],
+            // 'TRUE'
+            this.operations[1],
+            // 'FALSE'
+            this.operations[2]
+        ];
+    }
+}
+
+new Sample();
