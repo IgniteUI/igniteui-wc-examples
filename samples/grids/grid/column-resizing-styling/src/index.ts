c8c4970a4284df843a8239c0b83da30ff798fb27
Deyan Kamburov <dkamburov@infragistics.com>
2023-05-03 15:58:07 +0300

    add column resizing styling sample

---
+++ samples/grids/grid/column-resizing-styling/src/index.ts
@@ -0,0 +1,36 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcGridComponent, IgcColumnComponent } from 'igniteui-webcomponents-grids/grids';
+import { AthletesData } from './AthletesData';
+import "./index.css";
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import { ModuleManager } from 'igniteui-webcomponents-core';
+
+ModuleManager.register(
+    IgcPropertyEditorPanelModule
+);
+
+export class Sample {
+    private _bind: () => void;
+
+    constructor() {
+        var grid = document.getElementById('grid') as IgcGridComponent;
+
+        this._bind = () => {
+            grid.data = this.athletesData;
+        }
+        this._bind();
+
+    }
+
+    private _athletesData: AthletesData = null;
+    public get athletesData(): AthletesData {
+        if (this._athletesData == null)
+        {
+            this._athletesData = new AthletesData();
+        }
+        return this._athletesData;
+    }
+}
+
+new Sample();
