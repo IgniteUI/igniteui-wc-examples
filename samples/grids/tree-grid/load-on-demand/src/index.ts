f9069c4930d34f9cb3f1343afc12d366dc26b6f9
Maya Kirova <MKirova@infragistics.com>
2023-06-14 15:01:26 +0300

    Add Load on Demand Tree Grid sample.

---
+++ samples/grids/tree-grid/load-on-demand/src/index.ts
@@ -0,0 +1,38 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcTreeGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { EmployeesFlatDataItem, EmployeesFlatData } from './EmployeesFlatData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+export class Sample {
+
+    constructor() {
+        var treeGrid = document.getElementById('treeGrid') as IgcTreeGridComponent;
+        treeGrid.expansionDepth = 0;
+        const rootData = [...this.employeesFlatData].filter(x => x.ParentID == -1);
+        treeGrid.data = rootData;
+        treeGrid.loadChildrenOnDemand = (parentID: any, done: (children: any[]) => void) => {
+            this.getData(parentID, (children) => done(children));
+     };
+    }
+
+    public getData(parentID: any, done: (children: any[]) => void) {
+        setTimeout(() => {
+            const data = [...this.employeesFlatData];
+            const children = data.filter((r) => r.ParentID === parentID);
+            children.forEach((r) => r.hasEmployees = data.some((c) => c.ParentID === r.ID));
+            done(children);
+        }, 1000);
+    }
+
+    private _employeesFlatData: EmployeesFlatData = null;
+    public get employeesFlatData(): EmployeesFlatData {
+        if (this._employeesFlatData == null)
+        {
+            this._employeesFlatData = new EmployeesFlatData();
+        }
+        return this._employeesFlatData;
+    }
+
+}
+
+new Sample();
