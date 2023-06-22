1336b4501b2e672de09e7e2a108777716431f4a4
tfsbuild <tfsbuild@infragistics.com>
2023-06-22 11:38:50 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.6.22.3

--- samples/grids/grid/column-collapsible-groups/src/index.ts
+++ samples/grids/grid/column-collapsible-groups/src/index.ts
@@ -4,6 +4,8 @@ import { InvoicesDataItem, InvoicesData } from './InvoicesData';
 
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
 
+import "./index.css";
+
 export class Sample {
 
     private grid: IgcGridComponent
--- samples/grids/grid/data-performance-virtualization/src/index.ts
+++ samples/grids/grid/data-performance-virtualization/src/index.ts
@@ -4,6 +4,8 @@ import { FinancialDataAllItem, FinancialDataAll } from './FinancialDataAll';
 
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
 
+import "./index.css";
+
 export class Sample {
 
     private grid: IgcGridComponent
