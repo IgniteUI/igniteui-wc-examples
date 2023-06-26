ac7c413b2a59941e65dfb0056154b318a80baf89
HUSSAR-mtrela (Martin Trela) <HUSSAR-mtrela@users.noreply.github.com>
2023-06-26 13:34:27 -0400

    export samples (#493)

--- samples/charts/toolbar/layout-actions-for-data-chart/src/index.ts
+++ samples/charts/toolbar/layout-actions-for-data-chart/src/index.ts
@@ -1,8 +1,10 @@
 import { IgcToolbarModule } from 'igniteui-webcomponents-layouts';
-import { IgcDataChartToolbarModule, IgcDataChartCoreModule, IgcDataChartCategoryModule, IgcDataChartAnnotationModule, IgcDataChartInteractivityModule, IgcAnnotationLayerProxyModule, IgcDataChartCategoryTrendLineModule } from 'igniteui-webcomponents-charts';
+import { IgcDataChartToolbarModule, IgcDataChartCoreModule, IgcDataChartCategoryModule, IgcDataChartAnnotationModule, IgcDataChartInteractivityModule, IgcDataChartCategoryTrendLineModule } from 'igniteui-webcomponents-charts';
 import { IgcToolbarComponent } from 'igniteui-webcomponents-layouts';
 import { IgcDataChartComponent, IgcCategoryXAxisComponent, IgcNumericYAxisComponent, IgcLineSeriesComponent } from 'igniteui-webcomponents-charts';
 import { CountryRenewableElectricityItem, CountryRenewableElectricity } from './CountryRenewableElectricity';
+import { IgcToolCommandEventArgs } from 'igniteui-webcomponents-layouts';
+import { IgcSeriesComponent, IgcDataToolTipLayerComponent } from 'igniteui-webcomponents-charts';
 
 import { ModuleManager } from 'igniteui-webcomponents-core';
 
@@ -15,7 +17,6 @@ ModuleManager.register(
     IgcDataChartCategoryModule,
     IgcDataChartAnnotationModule,
     IgcDataChartInteractivityModule,
-    IgcAnnotationLayerProxyModule,
     IgcDataChartCategoryTrendLineModule
 );
 
@@ -32,6 +33,7 @@ export class Sample {
 
     constructor() {
         var toolbar = this.toolbar = document.getElementById('Toolbar') as IgcToolbarComponent;
+        this.toolbarToggleTooltip = this.toolbarToggleTooltip.bind(this);
         var chart = this.chart = document.getElementById('chart') as IgcDataChartComponent;
         var xAxis = this.xAxis = document.getElementById('xAxis') as IgcCategoryXAxisComponent;
         var yAxis = this.yAxis = document.getElementById('yAxis') as IgcNumericYAxisComponent;
@@ -41,6 +43,7 @@ export class Sample {
 
         this._bind = () => {
             toolbar.target = this.chart;
+            toolbar.onCommand = this.toolbarToggleTooltip;
             xAxis.dataSource = this.countryRenewableElectricity;
             lineSeries1.xAxis = this.xAxis;
             lineSeries1.yAxis = this.yAxis;
@@ -63,6 +66,36 @@ export class Sample {
             this._countryRenewableElectricity = new CountryRenewableElectricity();
         }
         return this._countryRenewableElectricity;
+    }
+
+
+    public toolbarToggleTooltip(sender: any, args: IgcToolCommandEventArgs): void {
+        var target = this.chart;
+        switch (args.command.commandId)
+    	{
+    		case "EnableTooltips":
+    			var enable = args.command.argumentsList[0].value as boolean;
+    			if (enable)
+    			{
+    				target.series.add(new IgcDataToolTipLayerComponent());
+    			}
+    			else
+    			{
+    				var toRemove = null;
+    				for (var i = 0; i < target.actualSeries.length; i++) {
+                        let s = target.actualSeries[i] as IgcSeriesComponent;
+    					if (s instanceof IgcDataToolTipLayerComponent)
+    					{
+    						toRemove = s;
+    					}
+    				}
+    				if (toRemove != null)
+    				{
+    					target.series.remove(toRemove);
+    				}
+    			}
+    			break;
+    	}
     }
 
 }
