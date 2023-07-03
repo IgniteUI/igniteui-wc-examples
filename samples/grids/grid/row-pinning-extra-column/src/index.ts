c4536cd60fb1f610550daee6800003165de85e1e
tfsbuild <tfsbuild@infragistics.com>
2023-07-03 10:35:57 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.7.3.3

--- samples/grids/grid/row-pinning-extra-column/src/index.ts
+++ samples/grids/grid/row-pinning-extra-column/src/index.ts
@@ -2,7 +2,7 @@ import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
 import 'igniteui-webcomponents-grids/grids/combined';
 import { ComponentRenderer, PropertyEditorPanelDescriptionModule, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
 import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
-import { CustomersDataItem, CustomersData } from './CustomersData';
+import CustomersDataLocal from './CustomersDataLocal.json';
 
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
 import { ModuleManager } from 'igniteui-webcomponents-core';
@@ -22,19 +22,15 @@ export class Sample {
         var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
 
         this._bind = () => {
-            grid.data = this.customersData;
+            grid.data = this.customersDataLocal;
         }
         this._bind();
 
     }
 
-    private _customersData: CustomersData = null;
-    public get customersData(): CustomersData {
-        if (this._customersData == null)
-        {
-            this._customersData = new CustomersData();
-        }
-        return this._customersData;
+    private _customersDataLocal: any[] = CustomersDataLocal;
+    public get customersDataLocal(): any[] {
+        return this._customersDataLocal;
     }
 
     private _componentRenderer: ComponentRenderer = null;
