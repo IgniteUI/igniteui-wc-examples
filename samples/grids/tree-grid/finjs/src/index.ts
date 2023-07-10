efa01f352d4acc2cbf3618a2ed72f7735f09c82e
Maya Kirova <MKirova@infragistics.com>
2023-07-10 18:34:40 +0300

    Remove groupby as it cannot be set in treegrid directly.

--- samples/grids/tree-grid/finjs/src/index.ts
+++ samples/grids/tree-grid/finjs/src/index.ts
@@ -21,23 +21,6 @@ export class Sample {
     private chart: IgcCategoryChartComponent;
     private _timer: ReturnType<typeof setInterval>;
     private data = FinancialData.generateData(1000);
-    private groupingExpr = [
-        {
-            dir: SortingDirection.Desc,
-            fieldName: 'category',
-            ignoreCase: false
-        },
-        {
-            dir: SortingDirection.Desc,
-            fieldName: 'type',
-            ignoreCase: false
-        },
-        {
-            dir: SortingDirection.Desc,
-            fieldName: 'contract',
-            ignoreCase: false
-        }
-    ];
     constructor() {
         const chart = this.chart = document.getElementById('chart1') as IgcCategoryChartComponent;
         var trendUp = `<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m123-240-43-43 292-291 167 167 241-241H653v-60h227v227h-59v-123L538-321 371-488 123-240Z"/></svg>`;
@@ -56,7 +39,6 @@ export class Sample {
         var changeP = document.getElementById('changeP') as IgcColumnComponent;
         var chartColumn = document.getElementById('chart') as IgcColumnComponent;
         grid1.data = this.data;
-        grid1.groupingExpressions = this.groupingExpr;
         price.bodyTemplate = this.priceTemplate;
         price.cellClasses = this.trends;
         changeP.cellClasses = this.trendsChange;
