98f984deb45bc068494b3731e17f624c2ffe906f
Simeon Simeonoff <sim.simeonoff@gmail.com>
2023-09-20 11:05:31 +0300

    refactor(stepper): update animation sample

--- samples/layouts/stepper/animations/src/index.ts
+++ samples/layouts/stepper/animations/src/index.ts
@@ -1,34 +1,41 @@
-import { defineComponents, IgcStepperComponent, IgcRadioGroupComponent, IgcStepComponent, IgcInputComponent, IgcButtonComponent, IgcRadioComponent } from "igniteui-webcomponents";
+import { defineComponents, IgcStepperComponent, IgcSelectComponent, IgcSelectItemComponent, IgcRadioGroupComponent, IgcInputComponent, IgcButtonComponent, IgcRadioComponent } from "igniteui-webcomponents";
 import "igniteui-webcomponents/themes/light/bootstrap.css";
-import "./StepperAnimations.css";
+import "./index.css";
 
-defineComponents(IgcStepperComponent, IgcRadioGroupComponent, IgcInputComponent, IgcButtonComponent);
+defineComponents(IgcStepperComponent, IgcRadioGroupComponent, IgcInputComponent, IgcButtonComponent, IgcSelectComponent);
 export class StepperAnimations {
     private stepper: IgcStepperComponent;
     private inputDuration: IgcInputComponent;
-    private radios: NodeListOf<IgcRadioComponent>;
 
     constructor() {
+        const orientation = document.getElementById("orientation") as IgcSelectComponent;
+        const horizontalAnim = document.getElementById("hanimation") as IgcSelectComponent;
+        const verticalAnim = document.getElementById("vanimation") as IgcSelectComponent;
+
+        horizontalAnim?.addEventListener("igcChange", (ev) => this.updateAnimations(ev, 'horizontal'));
+        verticalAnim?.addEventListener("igcChange", (ev) => this.updateAnimations(ev, 'vertical'));
+        orientation?.addEventListener("igcChange", (ev) => this.updateOrientation(ev));
+
         this.stepper = document.querySelector("igc-stepper") as IgcStepperComponent;
         this.inputDuration = document.getElementById("duration") as IgcInputComponent;
         this.inputDuration.addEventListener("igcChange", this.updateDuration.bind(this));
+    }
 
-        this.radios = document.querySelectorAll('igc-radio') as NodeListOf<IgcRadioComponent>;
-        this.radios.forEach(radio => {
-            radio.addEventListener('igcChange', () => {
-                if (radio!.parentElement!.id == "orientation") {
-                    this.stepper!.orientation = radio.value as "horizontal" | "vertical";
-                } else if (radio!.parentElement!.id == "vertical") {
-                    this.stepper!.verticalAnimation = radio.value as "grow" | "fade" | "none";
-                } else if (radio!.parentElement!.id == "horizontal") {
-                    this.stepper!.horizontalAnimation = radio.value as "slide" | "fade" | "none";
-                }
-            });
-        })
+    private updateOrientation(ev: CustomEvent) {
+        const orientation: any  = (ev.detail as IgcSelectItemComponent).value;
+        this.stepper.orientation = orientation;
+        console.log(orientation);
+    }
+
+    private updateAnimations(ev: CustomEvent, animationType: 'vertical' | 'horizontal') {
+        const animation: any  = (ev.detail as IgcSelectItemComponent).value;
+
+        if (animationType === 'horizontal') this.stepper.horizontalAnimation = animation;
+        if (animationType === 'vertical') this.stepper.verticalAnimation = animation;
     }
 
     private updateDuration() {
-        this.stepper.animationDuration = Number(this.inputDuration!.value);
+        this.stepper.animationDuration = Number(this.inputDuration.value);
     }
 }
 
