ed2896af9b9fed511d871caea305ff3f39a5f9c8
Maya <MKirova@infragistics.com>
2023-10-03 21:26:32 +0300

    Add custom toolbar content samples. (#469)
    * Add custom toolbar content example.
    
    * Add exporting visualization sample with custom button in toolbar.
    
    * Rename export visualization sample to match name in topic and xplat examples.
    
    ---------
    
    Co-authored-by: Svetoslav Krastev <SKrastev@infragistics.com>
    Co-authored-by: Svetoslav Krastev <striker18@mail.bg>
    Co-authored-by: HUSSAR-mtrela (Martin Trela) <HUSSAR-mtrela@users.noreply.github.com>

--- samples/grids/grid/data-exporting-indicator/src/index.ts
+++ samples/grids/grid/data-exporting-indicator/src/index.ts
@@ -1,12 +1,9 @@
 import 'igniteui-webcomponents-grids/grids/combined';
 import { ComponentRenderer, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
-import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { IgcGridComponent, IgcGridToolbarComponent } from 'igniteui-webcomponents-grids/grids';
 import { AthletesDataItem, AthletesData } from './AthletesData';
 import { defineComponents, IgcButtonComponent, IgcIconComponent, registerIconFromText } from "igniteui-webcomponents";
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
-
-import "./index.css";
-
 defineComponents(IgcButtonComponent, IgcIconComponent);
 export class Sample {
 
@@ -17,10 +14,14 @@ export class Sample {
         var icon = `<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>`;
         registerIconFromText("clear", icon, "material");
         var grid1 = this.grid1 = document.getElementById('grid1') as IgcGridComponent;
+        var toolbar = document.getElementById('toolbar') as IgcGridToolbarComponent;
         grid1.data = this.athletesData;
-        var button = document.getElementById('clearButton') as IgcButtonComponent;
+        var button = document.getElementById('simulate') as IgcButtonComponent;
         button.addEventListener("click", () => {
-            grid1.clearSort();
+            toolbar.showProgress = true;
+            setTimeout(() => {
+                toolbar.showProgress = false;
+            }, 5000);
         });
     }
 
