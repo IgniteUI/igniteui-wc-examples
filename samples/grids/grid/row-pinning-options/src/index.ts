5ee5ea75c9146fa11ef2937e40aa8b27cd5a6b8b
tfsbuild <tfsbuild@infragistics.com>
2023-10-09 10:14:32 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.10.9.3

--- samples/grids/grid/row-pinning-options/src/index.ts
+++ samples/grids/grid/row-pinning-options/src/index.ts
@@ -2,7 +2,7 @@ import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
 import 'igniteui-webcomponents-grids/grids/combined';
 import { ComponentRenderer, PropertyEditorPanelDescriptionModule, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
 import { IgcPropertyEditorPanelComponent, IgcPropertyEditorPropertyDescriptionComponent } from 'igniteui-webcomponents-layouts';
-import { IgcGridComponent, IgcActionStripComponent } from 'igniteui-webcomponents-grids/grids';
+import { IgcGridComponent, IgcPinningConfig, RowPinningPosition } from 'igniteui-webcomponents-grids/grids';
 import CustomersDataLocal from './CustomersDataLocal.json';
 
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
@@ -22,19 +22,29 @@ export class Sample {
     private propertyEditorPanel1: IgcPropertyEditorPanelComponent
     private rowPinningEditor: IgcPropertyEditorPropertyDescriptionComponent
     private grid: IgcGridComponent
-    private actionStrip: IgcActionStripComponent
+    private _pinningConfig1: IgcPinningConfig | null = null;
+    public get pinningConfig1(): IgcPinningConfig {
+        if (this._pinningConfig1 == null)
+        {
+            var pinningConfig1: IgcPinningConfig = {} as IgcPinningConfig;
+            pinningConfig1.rows = RowPinningPosition.Top;
+
+            this._pinningConfig1 = pinningConfig1;
+        }
+        return this._pinningConfig1;
+    }
     private _bind: () => void;
 
     constructor() {
         var propertyEditorPanel1 = this.propertyEditorPanel1 = document.getElementById('propertyEditorPanel1') as IgcPropertyEditorPanelComponent;
         var rowPinningEditor = this.rowPinningEditor = document.getElementById('rowPinningEditor') as IgcPropertyEditorPropertyDescriptionComponent;
         var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
-        var actionStrip = this.actionStrip = document.getElementById('actionStrip') as IgcActionStripComponent;
 
         this._bind = () => {
             propertyEditorPanel1.componentRenderer = this.renderer;
             propertyEditorPanel1.target = this.grid;
             grid.data = this.customersDataLocal;
+            grid.pinning = this.pinningConfig1;
         }
         this._bind();
 
