8e983c875093d683a36379cc3a148ba84a457f00
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2023-10-10 13:29:52 +0300

    Added textarea samples

---
+++ samples/inputs/textarea/overview/src/index.ts
@@ -0,0 +1,4 @@
+import { defineComponents, IgcTextareaComponent } from "igniteui-webcomponents";
+import "igniteui-webcomponents/themes/light/bootstrap.css";
+
+defineComponents(IgcTextareaComponent);
