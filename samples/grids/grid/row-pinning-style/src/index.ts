f9f4913b021598da9ba2dcf6313b742dd76262f9
tfsbuild <tfsbuild@infragistics.com>
2023-10-18 17:31:08 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.10.18.1

---
+++ samples/grids/grid/row-pinning-style/src/index.ts
@@ -0,0 +1,80 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { ComponentRenderer, WebGridDescriptionModule, WebActionStripDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcGridComponent, IgcPinningConfig, RowPinningPosition, IgcColumnComponent } from 'igniteui-webcomponents-grids/grids';
+import CustomersDataLocal from './CustomersDataLocal.json';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private grid: IgcGridComponent
+    private _pinningConfig1: IgcPinningConfig | null = null;
+    public get pinningConfig1(): IgcPinningConfig {
+        if (this._pinningConfig1 == null)
+        {
+            var pinningConfig1: IgcPinningConfig = {} as IgcPinningConfig;
+            pinningConfig1.rows = RowPinningPosition.Top;
+
+            this._pinningConfig1 = pinningConfig1;
+        }
+        return this._pinningConfig1;
+    }
+    private companyName: IgcColumnComponent
+    private contactName: IgcColumnComponent
+    private contactTitle: IgcColumnComponent
+    private address: IgcColumnComponent
+    private city: IgcColumnComponent
+    private postalCode: IgcColumnComponent
+    private phone: IgcColumnComponent
+    private fax: IgcColumnComponent
+    private _bind: () => void;
+
+    constructor() {
+        var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
+        this.webGridPinRowOnRendered = this.webGridPinRowOnRendered.bind(this);
+        var companyName = this.companyName = document.getElementById('CompanyName') as IgcColumnComponent;
+        var contactName = this.contactName = document.getElementById('ContactName') as IgcColumnComponent;
+        var contactTitle = this.contactTitle = document.getElementById('ContactTitle') as IgcColumnComponent;
+        var address = this.address = document.getElementById('Address') as IgcColumnComponent;
+        var city = this.city = document.getElementById('City') as IgcColumnComponent;
+        var postalCode = this.postalCode = document.getElementById('PostalCode') as IgcColumnComponent;
+        var phone = this.phone = document.getElementById('Phone') as IgcColumnComponent;
+        var fax = this.fax = document.getElementById('Fax') as IgcColumnComponent;
+
+        this._bind = () => {
+            grid.data = this.customersDataLocal;
+            grid.addEventListener("rendered", this.webGridPinRowOnRendered);
+            grid.pinning = this.pinningConfig1;
+        }
+        this._bind();
+
+    }
+
+    private _customersDataLocal: any[] = CustomersDataLocal;
+    public get customersDataLocal(): any[] {
+        return this._customersDataLocal;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            WebGridDescriptionModule.register(context);
+            WebActionStripDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+    public webGridPinRowOnRendered(args: any): void {
+        var grid = this.grid as any;
+        grid.data = [...grid.data];
+        grid.pinRow("ALFKI");
+        grid.pinRow("AROUT");
+    }
+
+}
+
+new Sample();
---
+++ samples/grids/tree-grid/excel-exporting/src/index.ts
@@ -0,0 +1,35 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcTreeGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { EmployeesNestedDataItem, EmployeesNestedDataItem_EmployeesItem, EmployeesNestedData } from './EmployeesNestedData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private treeGrid: IgcTreeGridComponent
+    private _bind: () => void;
+
+    constructor() {
+        var treeGrid = this.treeGrid = document.getElementById('treeGrid') as IgcTreeGridComponent;
+
+        this._bind = () => {
+            treeGrid.data = this.employeesNestedData;
+        }
+        this._bind();
+
+    }
+
+    private _employeesNestedData: EmployeesNestedData = null;
+    public get employeesNestedData(): EmployeesNestedData {
+        if (this._employeesNestedData == null)
+        {
+            this._employeesNestedData = new EmployeesNestedData();
+        }
+        return this._employeesNestedData;
+    }
+
+}
+
+new Sample();
