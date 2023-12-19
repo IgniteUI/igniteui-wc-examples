28dc8106cedb567ad5d996cba49f4a02f5c8162a
tfsbuild <tfsbuild@infragistics.com>
2023-12-19 17:06:55 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.12.19.2

---
+++ samples/grids/tree-grid/conditional-row-selectors/src/index.ts
@@ -0,0 +1,70 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { ComponentRenderer, WebTreeGridDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcTreeGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { EmployeesFlatDataItem, EmployeesFlatData } from './EmployeesFlatData';
+import { IgcRowSelectionEventArgs, IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private treeGrid: IgcTreeGridComponent
+    private _bind: () => void;
+
+    constructor() {
+        var treeGrid = this.treeGrid = document.getElementById('treeGrid') as IgcTreeGridComponent;
+        this.webTreeGridRowSelectionConditional = this.webTreeGridRowSelectionConditional.bind(this);
+
+        this._bind = () => {
+            treeGrid.data = this.employeesFlatData;
+            treeGrid.addEventListener("rowSelectionChanging", this.webTreeGridRowSelectionConditional);
+        }
+        this._bind();
+
+    }
+
+    private _employeesFlatData: EmployeesFlatData = null;
+    public get employeesFlatData(): EmployeesFlatData {
+        if (this._employeesFlatData == null)
+        {
+            this._employeesFlatData = new EmployeesFlatData();
+        }
+        return this._employeesFlatData;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            WebTreeGridDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+    public webTreeGridRowSelectionConditional(eventArgs: CustomEvent<IgcRowSelectionEventArgs>): void {
+        const event = eventArgs.detail;
+        if (!event.added.length && event.removed.length) {
+            // ignore de-select
+            return;
+        }
+        var grid = this.treeGrid;
+        const originalAddedLength = event.added.length;
+
+        // only allow selection for employees that are not pn PTO
+        event.newSelection = event.newSelection.filter(x => x.OnPTO === true);
+
+        // cleanup selection if all conditionally selectable rows are already selected
+        if (event.newSelection.length
+            && !event.newSelection.filter((x: any) => event.oldSelection.indexOf(x) === -1).length
+            && originalAddedLength > 1) {
+                // all selected from header, de-select instead
+                event.newSelection = [];
+        }
+    }
+
+}
+
+new Sample();
---
+++ samples/grids/tree-grid/filtering-options/src/index.ts
@@ -0,0 +1,35 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcTreeGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { OrdersDataItem, OrdersData } from './OrdersData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private treeGrid: IgcTreeGridComponent
+    private _bind: () => void;
+
+    constructor() {
+        var treeGrid = this.treeGrid = document.getElementById('treeGrid') as IgcTreeGridComponent;
+
+        this._bind = () => {
+            treeGrid.data = this.ordersData;
+        }
+        this._bind();
+
+    }
+
+    private _ordersData: OrdersData = null;
+    public get ordersData(): OrdersData {
+        if (this._ordersData == null)
+        {
+            this._ordersData = new OrdersData();
+        }
+        return this._ordersData;
+    }
+
+}
+
+new Sample();
