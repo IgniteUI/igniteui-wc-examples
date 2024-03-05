8c6bc0c47fc7b48266939fb343de0e1217c5a968
tfsbuild <tfsbuild@infragistics.com>
2024-03-05 10:21:42 -0500

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.3.5.1

---
+++ samples/charts/data-chart/data-tooltip-grouping-and-highlighting/src/index.ts
@@ -0,0 +1,70 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import { IgcDataChartCoreModule, IgcDataChartCategoryModule, IgcDataChartInteractivityModule, IgcDataChartAnnotationModule } from 'igniteui-webcomponents-charts';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, DataChartCoreDescriptionModule, DataChartCategoryDescriptionModule, DataChartInteractivityDescriptionModule, DataChartAnnotationDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcDataChartComponent, IgcCategoryXAxisComponent, IgcNumericYAxisComponent, IgcColumnSeriesComponent, IgcDataToolTipLayerComponent } from 'igniteui-webcomponents-charts';
+import { OlympicMedalsTopCountriesWithTotalsItem, OlympicMedalsTopCountriesWithTotals } from './OlympicMedalsTopCountriesWithTotals';
+
+import { ModuleManager } from 'igniteui-webcomponents-core';
+
+import "./index.css";
+
+ModuleManager.register(
+    IgcPropertyEditorPanelModule,
+    IgcDataChartCoreModule,
+    IgcDataChartCategoryModule,
+    IgcDataChartInteractivityModule,
+    IgcDataChartAnnotationModule
+);
+
+export class Sample {
+
+    private chart: IgcDataChartComponent
+    private xAxis: IgcCategoryXAxisComponent
+    private yAxis: IgcNumericYAxisComponent
+    private columnSeries1: IgcColumnSeriesComponent
+    private dataToolTipLayer: IgcDataToolTipLayerComponent
+    private _bind: () => void;
+
+    constructor() {
+        var chart = this.chart = document.getElementById('chart') as IgcDataChartComponent;
+        var xAxis = this.xAxis = document.getElementById('xAxis') as IgcCategoryXAxisComponent;
+        var yAxis = this.yAxis = document.getElementById('yAxis') as IgcNumericYAxisComponent;
+        var columnSeries1 = this.columnSeries1 = document.getElementById('ColumnSeries1') as IgcColumnSeriesComponent;
+        var dataToolTipLayer = this.dataToolTipLayer = document.getElementById('DataToolTipLayer') as IgcDataToolTipLayerComponent;
+
+        this._bind = () => {
+            xAxis.dataSource = this.olympicMedalsTopCountriesWithTotals;
+            columnSeries1.xAxis = this.xAxis;
+            columnSeries1.yAxis = this.yAxis;
+            columnSeries1.dataSource = this.olympicMedalsTopCountriesWithTotals;
+        }
+        this._bind();
+
+    }
+
+    private _olympicMedalsTopCountriesWithTotals: OlympicMedalsTopCountriesWithTotals = null;
+    public get olympicMedalsTopCountriesWithTotals(): OlympicMedalsTopCountriesWithTotals {
+        if (this._olympicMedalsTopCountriesWithTotals == null)
+        {
+            this._olympicMedalsTopCountriesWithTotals = new OlympicMedalsTopCountriesWithTotals();
+        }
+        return this._olympicMedalsTopCountriesWithTotals;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            DataChartCoreDescriptionModule.register(context);
+            DataChartCategoryDescriptionModule.register(context);
+            DataChartInteractivityDescriptionModule.register(context);
+            DataChartAnnotationDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
+new Sample();
