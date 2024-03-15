42742cae265cf800abbd3c29a434c1f59e81efeb
tfsbuild <tfsbuild@infragistics.com>
2024-03-15 04:53:18 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.3.15.1

---
+++ samples/grids/hierarchical-grid/column-resizing/src/index.ts
@@ -0,0 +1,35 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcHierarchicalGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { SingersDataItem, SingersDataItem_ToursItem, SingersDataItem_AlbumsItem, SingersDataItem_AlbumsItem_SongsItem, SingersData } from './SingersData';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private hierarchicalGrid1: IgcHierarchicalGridComponent
+    private _bind: () => void;
+
+    constructor() {
+        var hierarchicalGrid1 = this.hierarchicalGrid1 = document.getElementById('hierarchicalGrid1') as IgcHierarchicalGridComponent;
+
+        this._bind = () => {
+            hierarchicalGrid1.data = this.singersData;
+        }
+        this._bind();
+
+    }
+
+    private _singersData: SingersData = null;
+    public get singersData(): SingersData {
+        if (this._singersData == null)
+        {
+            this._singersData = new SingersData();
+        }
+        return this._singersData;
+    }
+
+}
+
+new Sample();
