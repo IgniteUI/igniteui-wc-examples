cbb95c9db6a33d98518c4f7740ac4599efdcf02f
tfsbuild <tfsbuild@infragistics.com>
2024-04-09 15:29:01 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.4.9.7

---
+++ samples/grids/hierarchical-grid/multi-column-headers-export/src/index.ts
@@ -0,0 +1,43 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcHierarchicalGridComponent, IgcGridToolbarExporterComponent } from 'igniteui-webcomponents-grids/grids';
+import MultiColumnsExportData from './MultiColumnsExportData.json';
+import { IgcExporterEvent } from 'igniteui-webcomponents-grids/grids';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private hierarchicalGrid1: IgcHierarchicalGridComponent
+    private hGridToolbarExporter: IgcGridToolbarExporterComponent
+    private _bind: () => void;
+
+    constructor() {
+        var hierarchicalGrid1 = this.hierarchicalGrid1 = document.getElementById('hierarchicalGrid1') as IgcHierarchicalGridComponent;
+        var hGridToolbarExporter = this.hGridToolbarExporter = document.getElementById('hGridToolbarExporter') as IgcGridToolbarExporterComponent;
+        this.webHierarchicalGridExportMultiColumnHeaders = this.webHierarchicalGridExportMultiColumnHeaders.bind(this);
+
+        this._bind = () => {
+            hierarchicalGrid1.data = this.multiColumnsExportData;
+            hGridToolbarExporter.addEventListener("exportStarted", this.webHierarchicalGridExportMultiColumnHeaders);
+        }
+        this._bind();
+
+    }
+
+    private _multiColumnsExportData: any[] = MultiColumnsExportData;
+    public get multiColumnsExportData(): any[] {
+        return this._multiColumnsExportData;
+    }
+
+
+    public webHierarchicalGridExportMultiColumnHeaders(args: CustomEvent<IgcExporterEvent>): void {
+        if (args.detail.options) {
+            args.detail.options.ignoreMultiColumnHeaders = false;
+        }
+    }
+
+}
+
+new Sample();
