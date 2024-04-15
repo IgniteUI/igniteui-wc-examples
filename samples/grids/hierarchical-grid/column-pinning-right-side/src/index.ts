0974fcd01657f35428af1085bad9017a47caf1c4
tfsbuild <tfsbuild@infragistics.com>
2024-04-15 09:19:54 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.4.15.5

---
+++ samples/grids/hierarchical-grid/column-pinning-right-side/src/index.ts
@@ -0,0 +1,43 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcHierarchicalGridComponent, IgcPinningConfig, ColumnPinningPosition } from 'igniteui-webcomponents-grids/grids';
+import HierarchicalCustomersData from './HierarchicalCustomersData.json';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private grid: IgcHierarchicalGridComponent
+    private _pinningConfig1: IgcPinningConfig | null = null;
+    public get pinningConfig1(): IgcPinningConfig {
+        if (this._pinningConfig1 == null)
+        {
+            var pinningConfig1: IgcPinningConfig = {} as IgcPinningConfig;
+            pinningConfig1.columns = ColumnPinningPosition.End;
+
+            this._pinningConfig1 = pinningConfig1;
+        }
+        return this._pinningConfig1;
+    }
+    private _bind: () => void;
+
+    constructor() {
+        var grid = this.grid = document.getElementById('grid') as IgcHierarchicalGridComponent;
+
+        this._bind = () => {
+            grid.data = this.hierarchicalCustomersData;
+            grid.pinning = this.pinningConfig1;
+        }
+        this._bind();
+
+    }
+
+    private _hierarchicalCustomersData: any[] = HierarchicalCustomersData;
+    public get hierarchicalCustomersData(): any[] {
+        return this._hierarchicalCustomersData;
+    }
+
+}
+
+new Sample();
---
+++ samples/grids/hierarchical-grid/custom-filtering/src/index.ts
@@ -0,0 +1,45 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcHierarchicalGridComponent, IgcColumnComponent } from 'igniteui-webcomponents-grids/grids';
+import SingersData from './SingersData.json';
+import { IgcCellTemplateContext } from 'igniteui-webcomponents-grids/grids';
+import { html, nothing } from 'lit-html';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private hierarchicalGrid1: IgcHierarchicalGridComponent
+    private column1: IgcColumnComponent
+    private _bind: () => void;
+
+    constructor() {
+        var hierarchicalGrid1 = this.hierarchicalGrid1 = document.getElementById('hierarchicalGrid1') as IgcHierarchicalGridComponent;
+        var column1 = this.column1 = document.getElementById('column1') as IgcColumnComponent;
+
+        this._bind = () => {
+            hierarchicalGrid1.data = this.singersData;
+            column1.bodyTemplate = this.webGridBooleanCellTemplate;
+        }
+        this._bind();
+
+    }
+
+    private _singersData: any[] = SingersData;
+    public get singersData(): any[] {
+        return this._singersData;
+    }
+
+
+        public webGridBooleanCellTemplate = (ctx: IgcCellTemplateContext) => {
+            if (ctx.cell.value) {
+                return html`<img src="https://www.infragistics.com/angular-demos-lob/assets/images/grid/active.png" title="Continued" alt="Continued" />`
+            } else {
+                return html`<img src="https://www.infragistics.com/angular-demos-lob/assets/images/grid/expired.png" title="Discontinued" alt="Discontinued" />`;
+            }
+    }
+
+}
+
+new Sample();
