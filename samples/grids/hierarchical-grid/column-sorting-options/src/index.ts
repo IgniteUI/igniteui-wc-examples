5ef6cb3067157c18642499c73cb5ed9ab8caed20
tfsbuild <tfsbuild@infragistics.com>
2024-04-15 07:41:05 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.4.15.4

---
+++ samples/grids/hierarchical-grid/column-sorting-options/src/index.ts
@@ -0,0 +1,47 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcHierarchicalGridComponent, IgcSortingExpression, SortingDirection } from 'igniteui-webcomponents-grids/grids';
+import SingersData from './SingersData.json';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+
+import "./index.css";
+
+export class Sample {
+
+    private hierarchicalGrid1: IgcHierarchicalGridComponent
+    private _sortingExpression1: IgcSortingExpression[] | null = null;
+    public get sortingExpression1(): IgcSortingExpression[] {
+        if (this._sortingExpression1 == null)
+        {
+            let sortingExpression1: IgcSortingExpression[] = [];
+            var sortingExpression2: IgcSortingExpression = {} as IgcSortingExpression;
+            sortingExpression2.fieldName = "Artist";
+            sortingExpression2.dir = SortingDirection.Asc;
+            sortingExpression2.ignoreCase = true;
+
+            sortingExpression1.push(sortingExpression2)
+            this._sortingExpression1 = sortingExpression1;
+        }
+        return this._sortingExpression1;
+    }
+    private _bind: () => void;
+
+    constructor() {
+        var hierarchicalGrid1 = this.hierarchicalGrid1 = document.getElementById('hierarchicalGrid1') as IgcHierarchicalGridComponent;
+
+        this._bind = () => {
+            hierarchicalGrid1.data = this.singersData;
+            hierarchicalGrid1.sortingExpressions = this.sortingExpression1;
+        }
+        this._bind();
+
+    }
+
+    private _singersData: any[] = SingersData;
+    public get singersData(): any[] {
+        return this._singersData;
+    }
+
+}
+
+new Sample();
---
+++ samples/grids/hierarchical-grid/row-selection-mode/src/index.ts
@@ -0,0 +1,61 @@
+import { IgcPropertyEditorPanelModule } from 'igniteui-webcomponents-layouts';
+import 'igniteui-webcomponents-grids/grids/combined';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, WebHierarchicalGridDescriptionModule } from 'igniteui-webcomponents-core';
+import { IgcPropertyEditorPanelComponent, IgcPropertyEditorPropertyDescriptionComponent } from 'igniteui-webcomponents-layouts';
+import { IgcHierarchicalGridComponent } from 'igniteui-webcomponents-grids/grids';
+import SingersData from './SingersData.json';
+
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import { defineAllComponents } from 'igniteui-webcomponents';
+import { ModuleManager } from 'igniteui-webcomponents-core';
+defineAllComponents();
+
+import "./index.css";
+
+ModuleManager.register(
+    IgcPropertyEditorPanelModule
+);
+
+export class Sample {
+
+    private propertyEditorPanel1: IgcPropertyEditorPanelComponent
+    private selectionType: IgcPropertyEditorPropertyDescriptionComponent
+    private hideRowSelectors: IgcPropertyEditorPropertyDescriptionComponent
+    private hierarchicalGrid: IgcHierarchicalGridComponent
+    private _bind: () => void;
+
+    constructor() {
+        var propertyEditorPanel1 = this.propertyEditorPanel1 = document.getElementById('propertyEditorPanel1') as IgcPropertyEditorPanelComponent;
+        var selectionType = this.selectionType = document.getElementById('selectionType') as IgcPropertyEditorPropertyDescriptionComponent;
+        var hideRowSelectors = this.hideRowSelectors = document.getElementById('hideRowSelectors') as IgcPropertyEditorPropertyDescriptionComponent;
+        var hierarchicalGrid = this.hierarchicalGrid = document.getElementById('hierarchicalGrid') as IgcHierarchicalGridComponent;
+
+        this._bind = () => {
+            propertyEditorPanel1.target = this.hierarchicalGrid;
+            propertyEditorPanel1.componentRenderer = this.renderer;
+            hierarchicalGrid.data = this.singersData;
+        }
+        this._bind();
+
+    }
+
+    private _singersData: any[] = SingersData;
+    public get singersData(): any[] {
+        return this._singersData;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            WebHierarchicalGridDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
+new Sample();
