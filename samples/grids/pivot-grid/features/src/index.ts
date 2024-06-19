b3a0d16e615631317cb2649481545fd80e5067a5
tfsbuild <tfsbuild@infragistics.com>
2024-06-19 14:44:24 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.6.19.2

--- samples/grids/pivot-grid/features/src/index.ts
+++ samples/grids/pivot-grid/features/src/index.ts
@@ -1,5 +1,5 @@
 import 'igniteui-webcomponents-grids/grids/combined';
-import { IgcPivotGridComponent, IgcPivotConfiguration, IgcPivotDateDimension, IgcPivotDimension, IgcPivotDateDimensionOptions, SortingDirection, IgcPivotValue, IgcPivotAggregator } from 'igniteui-webcomponents-grids/grids';
+import { IgcPivotGridComponent, IgcPivotConfiguration } from 'igniteui-webcomponents-grids/grids';
 import { PivotDataFlatItem, PivotDataFlat } from './PivotDataFlat';
 
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
@@ -14,76 +14,10 @@ export class Sample {
         if (this._pivotConfiguration1 == null)
         {
             var pivotConfiguration1: IgcPivotConfiguration = {} as IgcPivotConfiguration;
-
-            var pivotDateDimension1 = new IgcPivotDateDimension();
-            pivotDateDimension1.memberName = "Date";
-            pivotDateDimension1.enabled = true;
-            var pivotDimension1: IgcPivotDimension = {} as IgcPivotDimension;
-            pivotDimension1.memberName = "Date";
-            pivotDimension1.enabled = true;
-
-            pivotDateDimension1.baseDimension = pivotDimension1;
-            var pivotDateDimensionOptions1: IgcPivotDateDimensionOptions = {} as IgcPivotDateDimensionOptions;
-            pivotDateDimensionOptions1.years = true;
-            pivotDateDimensionOptions1.months = false;
-            pivotDateDimensionOptions1.quarters = true;
-            pivotDateDimensionOptions1.fullDate = false;
-
-            pivotDateDimension1.options = pivotDateDimensionOptions1;
-
-            pivotConfiguration1.columns = []
-            pivotConfiguration1.columns.push(pivotDateDimension1);
-            var pivotDimension2: IgcPivotDimension = {} as IgcPivotDimension;
-            pivotDimension2.memberName = "ProductName";
-            pivotDimension2.sortDirection = SortingDirection.Asc;
-            pivotDimension2.enabled = true;
-
-            pivotConfiguration1.rows = []
-            pivotConfiguration1.rows.push(pivotDimension2);
-            var pivotDimension3: IgcPivotDimension = {} as IgcPivotDimension;
-            pivotDimension3.memberName = "SellerCity";
-            pivotDimension3.enabled = true;
-
-            pivotConfiguration1.rows.push(pivotDimension3);
-            var pivotDimension4: IgcPivotDimension = {} as IgcPivotDimension;
-            pivotDimension4.memberName = "SellerName";
-            pivotDimension4.enabled = true;
-
-            pivotConfiguration1.filters = []
-            pivotConfiguration1.filters.push(pivotDimension4);
-            var pivotValue1: IgcPivotValue = {} as IgcPivotValue;
-            pivotValue1.member = "AmountofSale";
-            pivotValue1.displayName = "Amount of Sale";
-            pivotValue1.dataType = "currency";
-            pivotValue1.enabled = true;
-            var pivotAggregator1: IgcPivotAggregator = {} as IgcPivotAggregator;
-            pivotAggregator1.key = "SUM";
-            pivotAggregator1.label = "Sum of Sale";
-            pivotAggregator1.aggregator = this.pivotDataFlatAggregateSumSale;
-
-            pivotValue1.aggregate = pivotAggregator1;
-            var pivotAggregator2: IgcPivotAggregator = {} as IgcPivotAggregator;
-            pivotAggregator2.key = "SUM";
-            pivotAggregator2.label = "Sum of Sale";
-            pivotAggregator2.aggregator = this.pivotDataFlatAggregateSumSale;
-
-            pivotValue1.aggregateList = []
-            pivotValue1.aggregateList.push(pivotAggregator2);
-            var pivotAggregator3: IgcPivotAggregator = {} as IgcPivotAggregator;
-            pivotAggregator3.key = "MIN";
-            pivotAggregator3.label = "Minimum of Sale";
-            pivotAggregator3.aggregator = this.pivotDataFlatAggregateMinSale;
-
-            pivotValue1.aggregateList.push(pivotAggregator3);
-            var pivotAggregator4: IgcPivotAggregator = {} as IgcPivotAggregator;
-            pivotAggregator4.key = "MAX";
-            pivotAggregator4.label = "Maximum of Sale";
-            pivotAggregator4.aggregator = this.pivotDataFlatAggregateMaxSale;
-
-            pivotValue1.aggregateList.push(pivotAggregator4);
-
-            pivotConfiguration1.values = []
-            pivotConfiguration1.values.push(pivotValue1);
+            pivotConfiguration1.columns = [Infragistics.Controls.Description.CodeGenerationItemBuilder];
+            pivotConfiguration1.rows = [Infragistics.Controls.Description.CodeGenerationItemBuilder, Infragistics.Controls.Description.CodeGenerationItemBuilder];
+            pivotConfiguration1.filters = [Infragistics.Controls.Description.CodeGenerationItemBuilder];
+            pivotConfiguration1.values = [Infragistics.Controls.Description.CodeGenerationItemBuilder];
 
             this._pivotConfiguration1 = pivotConfiguration1;
         }
@@ -109,33 +43,6 @@ export class Sample {
             this._pivotDataFlat = new PivotDataFlat();
         }
         return this._pivotDataFlat;
-    }
-
-
-    public pivotDataFlatAggregateSumSale(members: any[], data: any[]): any {
-        return data.reduce((accumulator, value) => accumulator + value.ProductUnitPrice * value.NumberOfUnits, 0);
-    }
-
-    public pivotDataFlatAggregateMinSale(members: any[], data: any[]): any {
-        let min = 0;
-        if (data.length === 1) {
-            min = data[0].ProductUnitPrice * data[0].NumberOfUnits;
-        } else if (data.length > 1) {
-            const mappedData = data.map(x => x.ProductUnitPrice * x.NumberOfUnits);
-            min = mappedData.reduce((a, b) => Math.min(a, b));
-        }
-        return min;
-    }
-
-    public pivotDataFlatAggregateMaxSale(members: any[], data: any[]): any {
-        let max = 0;
-        if (data.length === 1) {
-            max = data[0].ProductUnitPrice * data[0].NumberOfUnits;
-        } else if (data.length > 1) {
-            const mappedData = data.map(x => x.ProductUnitPrice * x.NumberOfUnits);
-            max = mappedData.reduce((a, b) => Math.max(a, b));
-        }
-        return max;
     }
 
 }
