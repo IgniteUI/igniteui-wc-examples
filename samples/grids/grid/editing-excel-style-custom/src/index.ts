54749a3f39dd7f9bb25043c684463fdef3a46370
ttonev <ttonev@infragistics.com>
2024-07-10 22:25:50 +0300

    docs(grid): fixed keyboard nav and numeric edits

--- samples/grids/grid/editing-excel-style-custom/src/index.ts
+++ samples/grids/grid/editing-excel-style-custom/src/index.ts
@@ -1,6 +1,6 @@
 import 'igniteui-webcomponents-grids/grids/combined';
 import { ComponentRenderer, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
-import { IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
+import { IgcCellType, IgcGridComponent } from 'igniteui-webcomponents-grids/grids';
 import { NwindData } from './NwindData';
 
 import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
@@ -17,25 +17,61 @@ export class Sample {
     
         this._bind = () => {
             grid1.data = this.nwindData;
-            grid1.addEventListener('keydown', (event: KeyboardEvent) => {
-                var key = event.key; // Correct variable name used
-                const activeElem = grid1.selectedCells[0];
 
-                if ((key >= '0' && key <= '9') || (key.toLowerCase() >= 'a' && key.toLowerCase() <= 'z')) {
+            grid1.addEventListener('activeNodeChange', (event: any) => {
+                grid1.endEdit();
+                grid1.markForCheck();
+
+            });
+            grid1.addEventListener('keydown', (event: KeyboardEvent) => {
+                var key = event.key; 
+                var activeElem = grid1.selectedCells[0];
+
+                if(key === 'Escape') {
+                    activeElem.editMode = false;
+                    return;
+                }
+
+                if ((key >= '0' && key <= '9') || (key.toLowerCase() >= 'a' && key.toLowerCase() <= 'z') && key != 'Enter') {
         
                     if (activeElem && activeElem.editMode === false) {
-                        activeElem.setEditMode(true);
                         activeElem.value = key;
+                        
+                        activeElem.editMode = true;
+                        grid1.markForCheck();
                     }
+                        const inputElem = grid1.querySelector('input');
+                        if (inputElem) {
+                            if (inputElem.type === 'number') {
+                                inputElem.type = 'text';
+                                inputElem.setSelectionRange(inputElem.value.length, inputElem.value.length);
+                                inputElem.type = 'number';
+                            } else {
+                                inputElem.selectionStart = inputElem.selectionEnd = inputElem.value.length;
+                            }
+                        }
                 }
     
                 if (key === 'Enter') {
 
-                    var nextRow = this.getNextEditableRowIndex(activeElem.row.index, activeElem.row.cells, event.shiftKey);
+                    if(activeElem == null) {
+                        return;
+                    }
+                    var nextRowIndex = activeElem.row.index + 1;
+                    if(event.shiftKey) {
+                        nextRowIndex = activeElem.row.index - 1;
+                    }
+                    const maxRows = grid1.data.length;
+                    if (nextRowIndex >= maxRows) {
+                        nextRowIndex--;
+                    }
+                    if(nextRowIndex < 0) {
+                        nextRowIndex = 0;
+                    }
 
-                    grid1.navigateTo(nextRow, activeElem.column.index, (obj: any) => {    
-                        obj.target.activate();
-                        grid1.clearCellSelection();
+                    grid1.navigateTo(nextRowIndex, activeElem.column.visibleIndex, (obj: any) => {
+                        grid1.clearCellSelection(); 
+                        obj.target.activate(); 
                     });
 
             }
@@ -61,21 +97,6 @@ export class Sample {
             WebGridDescriptionModule.register(context);
         }
         return this._componentRenderer;
-    }
-
-    public getNextEditableRowIndex(currentRowIndex: number, dataView: any, previous: boolean) {
-        if (currentRowIndex < 0 || (currentRowIndex === 0 && previous) || (currentRowIndex >= dataView.length - 1 && !previous)) {
-            return currentRowIndex;
-        }
-        if (previous) {
-            return dataView.findLastIndex((rec: any, index: number) => index < currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
-        }
-        return dataView.findIndex((rec: any, index: number) => index > currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
-    }
-
-    public isEditableDataRecordAtIndex(dataViewIndex: number, dataView: any) {
-        const rec = dataView[dataViewIndex];
-        return !rec.expression && !rec.summaries && !rec.childGridsData && !rec.detailsData;
     }
 
 }
