75c6cb11582ecf558dc8df0ecb2ebfbe67175996
Deyan Kamburov <dkamburov@users.noreply.github.com>
2024-09-09 19:43:56 +0300

    chore(*): update ig packages to 5.0.0 (#844)

--- browser/tasks/gulp-samples.js
+++ browser/tasks/gulp-samples.js
@@ -976,17 +976,17 @@ function updateIG(cb) {
     // { name:               "igniteui-webcomponents-core", version: "3.2.2" },   // PUBLIC NPM
     let packageUpgrades = [
         // these IG packages are often updated:
-        { name: "igniteui-webcomponents-core"                     , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-charts"                   , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-excel"                    , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-gauges"                   , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-grids"                    , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-inputs"                   , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-layouts"                  , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-maps"                     , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-spreadsheet-chart-adapter", version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-spreadsheet"              , version: "5.0.0-beta.3" },
-        { name: "igniteui-webcomponents-datasources"              , version: "5.0.0-beta.3" },
+        { name: "igniteui-webcomponents-core"                     , version: "5.0.0" },
+        { name: "igniteui-webcomponents-charts"                   , version: "5.0.0" },
+        { name: "igniteui-webcomponents-excel"                    , version: "5.0.0" },
+        { name: "igniteui-webcomponents-gauges"                   , version: "5.0.0" },
+        { name: "igniteui-webcomponents-grids"                    , version: "5.0.0" },
+        { name: "igniteui-webcomponents-inputs"                   , version: "5.0.0" },
+        { name: "igniteui-webcomponents-layouts"                  , version: "5.0.0" },
+        { name: "igniteui-webcomponents-maps"                     , version: "5.0.0" },
+        { name: "igniteui-webcomponents-spreadsheet-chart-adapter", version: "5.0.0" },
+        { name: "igniteui-webcomponents-spreadsheet"              , version: "5.0.0" },
+        { name: "igniteui-webcomponents-datasources"              , version: "5.0.0" },
         // these IG packages are sometimes updated:
         { name: "igniteui-webcomponents", version: "5.0.0" },
         { name: "igniteui-dockmanager", version: "1.14.3" },
