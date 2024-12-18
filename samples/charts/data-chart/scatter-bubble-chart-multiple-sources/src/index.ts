57950dd878b74b1db71073a770eb78166223b067
tfsbuild <tfsbuild@infragistics.com>
2024-12-18 22:26:48 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.12.18.4

--- samples/charts/data-chart/scatter-bubble-chart-multiple-sources/src/index.ts
+++ samples/charts/data-chart/scatter-bubble-chart-multiple-sources/src/index.ts
@@ -30,7 +30,7 @@ export class Sample {
             var sizeScale1 = new IgcSizeScaleComponent();
             sizeScale1.isLogarithmic = false;
             sizeScale1.minimumValue = 10;
-            sizeScale1.maximumValue = 50;
+            sizeScale1.maximumValue = 80;
 
             this._sizeScale1 = sizeScale1;
         }
@@ -44,7 +44,7 @@ export class Sample {
             var sizeScale2 = new IgcSizeScaleComponent();
             sizeScale2.isLogarithmic = false;
             sizeScale2.minimumValue = 10;
-            sizeScale2.maximumValue = 50;
+            sizeScale2.maximumValue = 80;
 
             this._sizeScale2 = sizeScale2;
         }
