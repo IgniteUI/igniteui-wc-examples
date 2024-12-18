b430b2871cabdf2f0769b1819be8ba1a67f28a1c
tfsbuild <tfsbuild@infragistics.com>
2024-12-18 11:23:55 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.12.18.1

--- samples/charts/data-chart/scatter-bubble-chart-fill-scale/src/index.ts
+++ samples/charts/data-chart/scatter-bubble-chart-fill-scale/src/index.ts
@@ -42,8 +42,8 @@ export class Sample {
             var valueBrushScale1 = new IgcValueBrushScaleComponent();
             valueBrushScale1.isLogarithmic = false;
             valueBrushScale1.minimumValue = 0;
-            valueBrushScale1.maximumValue = 100000;
-            valueBrushScale1.brushes = ["rgba(26, 161, 226, 1)", "rgba(24, 154, 217, 1)", "rgba(22, 146, 206, 1)", "rgba(19, 133, 188, 1)", "rgba(15, 121, 171, 1)", "rgba(12, 107, 153, 1)", "rgba(9, 94, 136, 1)", "rgba(5, 82, 119, 1)", "rgba(2, 70, 105, 1)", "rgba(0, 63, 94, 1)"];
+            valueBrushScale1.maximumValue = 260000;
+            valueBrushScale1.brushes = ["rgba(137, 181, 250, 1)", "rgba(20, 108, 247, 1)", "rgba(82, 144, 242, 1)"];
 
             this._valueBrushScale1 = valueBrushScale1;
         }
