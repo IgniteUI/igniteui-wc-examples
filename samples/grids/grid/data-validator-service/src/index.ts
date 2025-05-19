45720b27e5d89e691be173a784adc5f8f91defae
tfsbuild <tfsbuild@infragistics.com>
2025-05-19 18:36:24 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.5.19.1

--- samples/grids/grid/data-validator-service/src/index.ts
+++ samples/grids/grid/data-validator-service/src/index.ts
@@ -24,12 +24,8 @@ export class Sample {
     private propertyEditor: IgcPropertyEditorPanelComponent
     private rowEditableEditor: IgcPropertyEditorPropertyDescriptionComponent
     private grid: IgcGridComponent
-    private avatar: IgcColumnComponent
-    private name: IgcColumnComponent
-    private company: IgcColumnComponent
-    private email: IgcColumnComponent
-    private fax: IgcColumnComponent
-    private createdOn: IgcColumnComponent
+    private column1: IgcColumnComponent
+    private column2: IgcColumnComponent
     private _columnPipeArgs1: IgcColumnPipeArgs | null = null;
     public get columnPipeArgs1(): IgcColumnPipeArgs {
         if (this._columnPipeArgs1 == null)
@@ -41,35 +37,21 @@ export class Sample {
         }
         return this._columnPipeArgs1;
     }
-    private lastActivity: IgcColumnComponent
-    private estimatedSales: IgcColumnComponent
-    private dealsLost: IgcColumnComponent
-    private dealsWon: IgcColumnComponent
-    private dealsPending: IgcColumnComponent
     private _bind: () => void;
 
     constructor() {
         var propertyEditor = this.propertyEditor = document.getElementById('PropertyEditor') as IgcPropertyEditorPanelComponent;
         var rowEditableEditor = this.rowEditableEditor = document.getElementById('RowEditableEditor') as IgcPropertyEditorPropertyDescriptionComponent;
         var grid = this.grid = document.getElementById('grid') as IgcGridComponent;
-        var avatar = this.avatar = document.getElementById('Avatar') as IgcColumnComponent;
-        var name = this.name = document.getElementById('Name') as IgcColumnComponent;
-        var company = this.company = document.getElementById('Company') as IgcColumnComponent;
-        var email = this.email = document.getElementById('Email') as IgcColumnComponent;
-        var fax = this.fax = document.getElementById('Fax') as IgcColumnComponent;
-        var createdOn = this.createdOn = document.getElementById('CreatedOn') as IgcColumnComponent;
-        var lastActivity = this.lastActivity = document.getElementById('LastActivity') as IgcColumnComponent;
-        var estimatedSales = this.estimatedSales = document.getElementById('EstimatedSales') as IgcColumnComponent;
-        var dealsLost = this.dealsLost = document.getElementById('DealsLost') as IgcColumnComponent;
-        var dealsWon = this.dealsWon = document.getElementById('DealsWon') as IgcColumnComponent;
-        var dealsPending = this.dealsPending = document.getElementById('DealsPending') as IgcColumnComponent;
+        var column1 = this.column1 = document.getElementById('column1') as IgcColumnComponent;
+        var column2 = this.column2 = document.getElementById('column2') as IgcColumnComponent;
 
         this._bind = () => {
             propertyEditor.componentRenderer = this.renderer;
             propertyEditor.target = this.grid;
             grid.data = this.employeesData;
-            avatar.bodyTemplate = this.webGridAvatarCellTemplate;
-            createdOn.pipeArgs = this.columnPipeArgs1;
+            column1.bodyTemplate = this.webGridAvatarCellTemplate;
+            column2.pipeArgs = this.columnPipeArgs1;
         }
         this._bind();
 
