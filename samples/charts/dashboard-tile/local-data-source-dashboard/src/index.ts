939a95b60af1876dbd2d388eb558ff3cadd5e8a5
tfsbuild <tfsbuild@infragistics.com>
2025-05-21 18:02:25 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.5.21.2

---
+++ samples/charts/dashboard-tile/local-data-source-dashboard/src/index.ts
@@ -0,0 +1,44 @@
+import { IgcDashboardTileModule, IgcDataChartDashboardTileModule, IgcGeographicMapDashboardTileModule, IgcLinearGaugeDashboardTileModule, IgcPieChartDashboardTileModule, IgcRadialGaugeDashboardTileModule } from 'igniteui-webcomponents-dashboards';
+import { IgcDashboardTileComponent } from 'igniteui-webcomponents-dashboards';
+import { RetailSalesPerformanceLocalDataSource } from './RetailSalesPerformanceLocalDataSource';
+
+import { ModuleManager } from 'igniteui-webcomponents-core';
+
+import "./index.css";
+
+ModuleManager.register(
+    IgcDashboardTileModule,
+    IgcDataChartDashboardTileModule,
+    IgcGeographicMapDashboardTileModule,
+    IgcLinearGaugeDashboardTileModule,
+    IgcPieChartDashboardTileModule,
+    IgcRadialGaugeDashboardTileModule
+);
+
+export class Sample {
+
+    private dashboard: IgcDashboardTileComponent
+    private _bind: () => void;
+
+    constructor() {
+        var dashboard = this.dashboard = document.getElementById('dashboard') as IgcDashboardTileComponent;
+
+        this._bind = () => {
+            dashboard.dataSource = this.retailSalesPerformanceLocalDataSource;
+        }
+        this._bind();
+
+    }
+
+    private _retailSalesPerformanceLocalDataSource: RetailSalesPerformanceLocalDataSource = null;
+    public get retailSalesPerformanceLocalDataSource(): RetailSalesPerformanceLocalDataSource {
+        if (this._retailSalesPerformanceLocalDataSource == null)
+        {
+            this._retailSalesPerformanceLocalDataSource = new RetailSalesPerformanceLocalDataSource();
+        }
+        return this._retailSalesPerformanceLocalDataSource;
+    }
+
+}
+
+new Sample();
