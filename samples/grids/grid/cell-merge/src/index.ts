e7bf37094d7bf9f4b2ccdc9e8b94e7e10e1389e9
Ivan Minchev <IMinchev@infragistics.com>
2025-10-27 14:19:40 +0200

    feat(grid): add cell merge sample

---
+++ samples/grids/grid/cell-merge/src/index.ts
@@ -0,0 +1,60 @@
+import 'igniteui-webcomponents-grids/grids/combined';
+import { IgcGridComponent, IgcColumnComponent, IgcCellTemplateContext } from 'igniteui-webcomponents-grids/grids';
+import { html } from 'lit-html';
+import { InvoicesData } from './InvoicesData';
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import { defineAllComponents, IgcSelectComponent } from 'igniteui-webcomponents';
+
+defineAllComponents();
+
+import "./index.css";
+
+export class Sample {
+    private grid: IgcGridComponent;
+    private _bind: () => void;
+
+    constructor() {
+        const grid = this.grid = document.getElementById('grid') as IgcGridComponent;
+
+        this._bind = (): void => {
+            grid.data = this.invoicesData;
+            grid.addEventListener('columnInit', this.onColumnInit);
+
+            const mergeSelect = document.getElementById('mergeSelect') as IgcSelectComponent;
+            mergeSelect?.addEventListener('igcChange', (e: CustomEvent) => {
+                const value = (e as any).detail.value as 'always' | 'onSort';
+                (grid as any).cellMergeMode = value;
+            });
+        };
+
+        this._bind();
+    }
+
+    private _invoicesData: InvoicesData = null;
+    public get invoicesData(): InvoicesData {
+        if (this._invoicesData == null) {
+            this._invoicesData = new InvoicesData();
+        }
+        return this._invoicesData;
+    }
+
+    private onColumnInit = (args: CustomEvent) => {
+        const column = args.detail as IgcColumnComponent;
+        if (column.field === 'UnitPrice') {
+            column.formatter = (val: number) => "$" + Number(val).toFixed(2);
+        }
+        if (column.field === 'Discontinued') {
+            column.bodyTemplate = this.discontinuedTemplate;
+        }
+    };
+
+    private discontinuedTemplate = (ctx: IgcCellTemplateContext) => {
+        if (ctx.cell.value) {
+            return html`<img src="https://static.infragistics.com/xplatform/images/grid/active.png" title="Continued" alt="Continued" />`;
+        }
+        return html`<img src="https://static.infragistics.com/xplatform/images/grid/expired.png" title="Discontinued" alt="Discontinued" />`;
+    };
+}
+
+new Sample();
