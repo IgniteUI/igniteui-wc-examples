15606ee12195eea5e22203fc96533e089b5b1926
Stamen Stoychev <SStoychev@infragistics.com>
2026-02-04 17:09:20 +0200

    chore(*): making vite browser addressable from root

--- browser-vite/vite.config.js
+++ browser-vite/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  root: '.',
+  root: path.resolve(__dirname),
   base: './',
   publicDir: 'public',
   build: {
@@ -31,7 +35,7 @@ export default defineConfig({
       scss: {
         api: 'modern-compiler',
         loadPaths: [
-          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, '..', 'node_modules'),
           path.resolve(__dirname, 'src')
         ]
       }
