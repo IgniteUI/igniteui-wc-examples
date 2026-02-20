d9fcd9fb0dc923fdde404c9cacac15c9f71ecbe8
Stamen Stoychev <SStoychev@infragistics.com>
2026-02-20 21:20:16 +0200

    fix(grid-lite): updating for latest grid lite v

--- samples/grids/grid-lite/styling-config-themes/src/index.ts
+++ samples/grids/grid-lite/styling-config-themes/src/index.ts
@@ -1,6 +1,7 @@
 import { IgcGridLite } from 'igniteui-grid-lite';
 import { defineComponents, IgcRatingComponent, IgcSelectComponent } from 'igniteui-webcomponents';
 import { GridLiteDataService, ProductInfo } from './GridLiteDataService';
+import { html, render } from 'lit-html';
 import "igniteui-webcomponents/themes/light/bootstrap.css";
 import "./index.scss";
 
@@ -9,59 +10,54 @@ defineComponents(IgcRatingComponent, IgcSelectComponent);
 
 export class Sample {
     private dataService: GridLiteDataService;
+    private formatter: Intl.NumberFormat;
     private currentTheme: string = 'bootstrap-light';
     private data: ProductInfo[] = [];
 
     constructor() {
         this.dataService = new GridLiteDataService();
-        const gridLite = document.getElementById('grid-lite') as any;
-        this.data = this.dataService.generateProducts(50);
-        
-        const columns = [
-            { 
-                key: 'name', 
-                headerText: 'Product', 
-                sort: true, 
-                filter: true 
-            },
-            {
-                key: 'price',
-                headerText: 'Price',
-                sort: true,
-                filter: true,
-                type: 'number'
-            },
-            {
-                key: 'sold',
-                headerText: 'Sold',
-                sort: true,
-                filter: true,
-                type: 'number'
-            },
-            {
-                key: 'total',
-                headerText: 'Total',
-                sort: true,
-                filter: true,
-                type: 'number'
-            },
-            {
-                key: 'rating',
-                headerText: 'Rating',
-                type: 'number',
-                sort: true,
-                filter: true,
-                cellTemplate: (params: any) => {
-                    const rating = document.createElement('igc-rating');
-                    rating.setAttribute('readonly', '');
-                    rating.setAttribute('value', params.value.toString());
-                    return rating;
-                }
-            }
-        ];
+        this.formatter = new Intl.NumberFormat('en-EN', {
+            style: 'currency',
+            currency: 'EUR'
+        });
 
-        gridLite.columns = columns;
-        gridLite.data = this.data;
+        const data: ProductInfo[] = this.dataService.generateProducts(50);
+
+        const container = document.getElementById('grid-lite');
+
+        const template = html`
+            <igc-grid-lite .data=${data}>
+                <igc-grid-lite-column 
+                field="name" 
+                header="Product Name"
+                ></igc-grid-lite-column>
+                <igc-grid-lite-column 
+                field="price" 
+                header="Price" 
+                data-type="number"
+                .cellTemplate=${(params: any) => html`<span>${this.formatter.format(params.value)}</span>`}
+                ></igc-grid-lite-column>
+                <igc-grid-lite-column 
+                field="sold" 
+                header="Units sold" 
+                data-type="number"
+                ></igc-grid-lite-column>
+                <igc-grid-lite-column 
+                field="total" 
+                header="Total sold"
+                .cellTemplate=${(params: any) => html`<span>${this.formatter.format(params.value)}</span>`}
+                ></igc-grid-lite-column>
+                <igc-grid-lite-column 
+                field="rating" 
+                header="Customer rating" 
+                data-type="number"
+                .cellTemplate=${(params: any) => html`<igc-rating readonly step="0.01" value=${params.value}></igc-rating>`}
+                ></igc-grid-lite-column>
+            </igc-grid-lite>
+        `;
+
+        render(template, container!);
+
 
         // Theme switcher
         const themeSelect = document.getElementById('theme-select') as IgcSelectComponent;
