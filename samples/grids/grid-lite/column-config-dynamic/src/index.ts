2f91db1ad8f05b69e97da937a5730daa72d22330
Stamen Stoychev <sstoychev@infragistics.com>
2026-02-20 15:43:44 +0200

    feat(*): updating staging with latest lib versions (#1163)

--- samples/grids/grid-lite/column-config-dynamic/src/index.ts
+++ samples/grids/grid-lite/column-config-dynamic/src/index.ts
@@ -1,4 +1,4 @@
-import { ColumnConfiguration, IgcGridLite } from 'igniteui-grid-lite';
+import { IgcGridLite } from 'igniteui-grid-lite';
 import { defineComponents, IgcCheckboxChangeEventArgs, IgcCheckboxComponent, IgcDropdownComponent, IgcRatingComponent, IgcSwitchComponent } from 'igniteui-webcomponents';
 import { GridLiteDataService, ProductInfo } from './GridLiteDataService';
 
@@ -18,11 +18,18 @@ const formatter = new Intl.NumberFormat('en-EN', {
 
 export class Sample {
     private dataService: GridLiteDataService;
-    private gridLite: any;
+    private gridLite: IgcGridLite<ProductInfo>;
     private dropdown: IgcDropdownComponent;
     private formattersSwitch: IgcSwitchComponent;
     protected hasFormatters = true;
     protected format = (params: any) => formatter.format(params.value);
+    protected rating: (params: any) => HTMLElement = (params: any) => {
+        const rating = document.createElement('igc-rating');
+        rating.setAttribute('readonly', '');
+        rating.setAttribute('step', '0.01');
+        rating.setAttribute('value', params.value.toString());
+        return rating;
+    }
     private columns: any[] = [];
 
     constructor() {
@@ -33,65 +40,13 @@ export class Sample {
 
         const data: ProductInfo[] = this.dataService.generateProducts(50);
 
-        this.columns = [
-            {
-                key: 'id',
-                hidden: true,
-                headerText: 'ID'
-            },
-            {
-                key: 'name',
-                headerText: 'Product Name'
-            },
-            {
-                key: 'price',
-                headerText: 'Price',
-                type: 'number',
-                cellTemplate: (params: any) => {
-                    const span = document.createElement('span');
-                    span.textContent = formatter.format(params.value);
-                    return span;
-                }
-            },
-            {
-                key: 'sold',
-                type: 'number',
-                headerText: 'Units sold'
-            },
-            {
-                key: 'total',
-                headerText: 'Total sold',
-                cellTemplate: (params: any) => {
-                    const span = document.createElement('span');
-                    span.textContent = formatter.format(params.value);
-                    return span;
-                }
-            },
-            {
-                key: 'rating',
-                type: 'number',
-                headerText: 'Customer rating',
-                cellTemplate: (params: any) => {
-                    const rating = document.createElement('igc-rating');
-                    rating.setAttribute('readonly', '');
-                    rating.setAttribute('step', '0.01');
-                    rating.setAttribute('value', params.value.toString());
-                    return rating;
-                }
-            }
-        ];
-
-        this.gridLite.columns = this.columns;
         this.gridLite.data = data;
 
         this.dropdown.addEventListener('igcChange', (e: any) => { this.dropdown.clearSelection(); });
         this.formattersSwitch.addEventListener('igcChange', (e: CustomEvent<IgcCheckboxChangeEventArgs>) => {
-            this.gridLite.updateColumns(
-                ['price', 'total'].map((key) => ({
-                    key,
-                    cellTemplate: e.detail.checked ? this.format : undefined,
-                })) as ColumnConfiguration<ProductInfo>[]
-            );
+              this.gridLite.getColumn('price').cellTemplate = e.detail.checked ? this.format : undefined; 
+              this.gridLite.getColumn('total').cellTemplate = e.detail.checked ? this.format : undefined;
+              //this.gridLite.columns = [...this.gridLite.columns];
         });
 
         document.addEventListener('click', (e: MouseEvent) => {
@@ -101,7 +56,7 @@ export class Sample {
                 const dropdownItem = checkbox.closest('igc-dropdown-item');
                 const columnKey = dropdownItem?.id;
                 const prop = Array.from(checkbox.classList).filter(c => c.endsWith('-checkbox'))[0].split('-')[0];
-                this.gridLite.updateColumns({ key: columnKey, [prop]: !checkbox.checked });
+               // this.gridLite.updateColumns({ key: columnKey, [prop]: !checkbox.checked });
             }
         });
     }
