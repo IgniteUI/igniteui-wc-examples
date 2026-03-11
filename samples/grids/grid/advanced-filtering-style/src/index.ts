c68ab4e182cfc66ebbc8a8405821d877e5c691a1
didimmova <d.dimova11@gmail.com>
2026-03-11 16:24:45 +0200

    fix(sample): fix inputs in query-builder not getting bootstrap styles

--- samples/grids/grid/advanced-filtering-style/src/index.ts
+++ samples/grids/grid/advanced-filtering-style/src/index.ts
@@ -1,11 +1,11 @@
+import "igniteui-webcomponents/themes/light/bootstrap.css";
+import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
 import 'igniteui-webcomponents-grids/grids/combined';
 import { ComponentRenderer, WebGridDescriptionModule } from 'igniteui-webcomponents-core';
 import { IgcGridComponent, IgcColumnComponent } from 'igniteui-webcomponents-grids/grids';
 import NwindData from './NwindData.json';
 import { IgcCellTemplateContext } from 'igniteui-webcomponents-grids/grids';
 import { html, nothing } from 'lit-html';
-
-import "igniteui-webcomponents-grids/grids/themes/light/bootstrap.css";
 
 import "./index.css";
 
