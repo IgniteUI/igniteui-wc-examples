890d13a34910dfe38a786babe8cba1fba8ca5d82
Hristo Hristov <HHristov@infragistics.com>
2026-07-27 16:21:16 +0300

    feat(docs): update badge samples

---
+++ samples/inputs/badge/type/src/index.ts
@@ -0,0 +1,5 @@
+import { defineComponents, IgcBadgeComponent } from 'igniteui-webcomponents';
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import './index.css';
+
+defineComponents(IgcBadgeComponent);
