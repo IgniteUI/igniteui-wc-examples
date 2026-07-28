cacc2e0394c32e6aa976c4e97c1bf61eff1c7d9d
Hristo Hristov <HHristov@infragistics.com>
2026-07-28 17:08:38 +0300

    feat(button-group): update button group samples

---
+++ samples/inputs/button-group/layout/src/index.ts
@@ -0,0 +1,5 @@
+import { defineComponents, IgcButtonGroupComponent, IgcRippleComponent, IgcToggleButtonComponent } from 'igniteui-webcomponents';
+import 'igniteui-webcomponents/themes/light/material.css';
+import './index.css';
+
+defineComponents(IgcButtonGroupComponent, IgcRippleComponent, IgcToggleButtonComponent);
