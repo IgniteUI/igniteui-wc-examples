dac65adc36bd4064bef53302a6734dd9f9185d5d
Hristo Hristov <HHristov@infragistics.com>
2026-09-08 14:14:35 +0300

    fix(button-group): update samples to use bootstrap theme

--- samples/inputs/button-group/interaction-states/src/index.ts
+++ samples/inputs/button-group/interaction-states/src/index.ts
@@ -6,7 +6,7 @@ import {
     IgcToggleButtonComponent,
     registerIconFromText
 } from 'igniteui-webcomponents';
-import 'igniteui-webcomponents/themes/light/material.css';
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
 import './index.css';
 
 defineComponents(IgcButtonGroupComponent, IgcIconComponent, IgcRippleComponent, IgcToggleButtonComponent);
