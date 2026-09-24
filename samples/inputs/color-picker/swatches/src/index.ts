de0ebec0c9f4eaeef653f1791368d39710a1e7d7
Adrian Petrov <adrian.ptrv1@gmail.com>
2026-09-24 10:28:41 +0300

    fix(): add additional swatches line

--- samples/inputs/color-picker/swatches/src/index.ts
+++ samples/inputs/color-picker/swatches/src/index.ts
@@ -7,10 +7,11 @@ defineComponents(IgcColorPickerComponent);
 export class ColorPickerSwatches {
     constructor() {
         document.querySelector('#one-line')!.swatches = ['#f44336', '#e91e63', '#9c27b0', '#3f51b5', '#2196f3', '#4caf50'];
-        document.querySelector('#multi-line')!.swatches = ['#f44336', '#e91e63', '#9c27b0', '#3f51b5', '#2196f3', '#4caf50', '#ffeb3b', '#ff9800', '#795548', '#607d8b', '#ffffff', '#000000', '#0000ff', '#00ff00', '#ff00ff', '#00ffff'];
+        document.querySelector('#multi-line')!.swatches = ['#f44336', '#e91e63', '#9c27b0', '#3f51b5', '#2196f3', '#4caf50', '#ffeb3b', '#ff9800', '#795548', '#607d8b', '#ffffff', '#000000', '#0000ff', '#00ff00', '#ff00ff', '#00ffff', '#ff0000', '#ffff00', '#ff00ff', '#00ffff', '#c0c0c0', '#808080', '#800000', '#808000'];
     }
 }
 
 document.querySelectorAll('igc-color-picker')?.forEach(picker => picker.toggle());
 
 new ColorPickerSwatches();
+h
